a8fae3e2c031df6457243e96791d1239dca5e200
Afina <afinawalton95@gmail.com>
2021-09-27 13:44:38 -0700

    Adds constructor to Item class and get_by_category() to Vendor class

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -17,4 +17,13 @@ class Vendor:
             return False
         return item
 
-    
+    def get_by_category(self, category):
+        # Return a list of items that match category
+        # The vendor has an inventory list of items which have categories attached
+        items = [] # Creates empty list to return
+
+        for item in self.inventory: # For each item in inventory list
+            if item.category == category: # That item represents an Item instance, with a category attribute
+                items.append(item) # Append the whole item to the list
+
+        return items
