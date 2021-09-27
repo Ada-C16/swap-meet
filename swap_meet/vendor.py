0b56df753f5d9d4bc2d85fef32ffa3561712ae9a
ayaka <ayakaob@gmail.com>
2021-09-27 15:50:04 -0700

    pass all tests of wave 2

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -12,3 +12,6 @@ class Vendor:
         except:
             return False
         return item
+
+    def get_by_category(self, category):
+        return [item for item in self.inventory if item.category == category]
