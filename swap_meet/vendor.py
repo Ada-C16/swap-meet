979e7503c32463eac682305810f9ecbbd92f6764
lain <lain.winslow@gmail.com>
2021-09-27 16:56:07 -0700

    rename args other_vendor to other for consistency

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -15,17 +15,17 @@ class Vendor:
     def get_by_category(self, category):
         return [item for item in self.inventory if item.category == category]
 
-    def swap_items(self, other_vendor, my_item, their_item):
-        if my_item in self.inventory and their_item in other_vendor.inventory:
-            other_vendor.add(self.remove(my_item))
-            self.add(other_vendor.remove(their_item))
+    def swap_items(self, other, my_item, their_item):
+        if my_item in self.inventory and their_item in other.inventory:
+            other.add(self.remove(my_item))
+            self.add(other.remove(their_item))
             return True
         return False
     
-    def swap_first_item(self, other_vendor):
+    def swap_first_item(self, other):
         my_first_item = next(iter(self.inventory),None)
-        their_first_item = next(iter(other_vendor.inventory),None)
-        return self.swap_items(other_vendor,my_first_item,their_first_item)
+        their_first_item = next(iter(other.inventory),None)
+        return self.swap_items(other,my_first_item,their_first_item)
     
     def get_best_by_category(self, category):
         items_in_category = (item for item in self.inventory if item.category == category)
