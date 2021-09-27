45b2eef2ff7a1917a0ddcf6911dda0a97f9a6f37
Lux Barker <lux.barker13@gmail.com>
2021-09-27 13:44:33 -0700

    implement Vendor.add() to add new item to inventory

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -2,3 +2,7 @@
 class Vendor:
     def __init__(self, inventory=None):
         self.inventory = inventory if inventory else []
+
+    def add(self, item):
+        self.inventory.append(item)
+        return item
