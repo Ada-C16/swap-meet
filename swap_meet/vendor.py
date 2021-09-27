a6675d21bc7b4b0c1963c156dd7280fcee291d30
Lain Winslow <lain.winslow@gmail.com>
2021-09-27 14:56:15 -0700

    wave 1 tests pass

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,13 @@
 class Vendor:
-    pass
+    def __init__(self, inventory=None):
+        self.inventory = inventory or []
+
+    def add(self, new_item):
+        self.inventory.append(new_item)
+        return new_item
+
+    def remove(self, to_remove):
+        if to_remove not in self.inventory:
+            return False
+        self.inventory.remove(to_remove)
+        return to_remove
