c0b5df01c11d3ed96592c06d39f51dd32b0741b7
Mac <mackenziespeyton@gmail.com>
2021-09-27 13:56:14 -0700

    ICreated Vendor class with inventory attribute, add method, remove method. Passing Wave 01 tests.

---
+++ main.py
@@ -0,0 +1,8 @@
+from swap_meet.vendor import Vendor
+from swap_meet.item import Item
+
+vendor = Vendor()
+item = Item()
+
+print(vendor)
+print(item)
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,18 @@
 class Vendor:
-    pass
+    def __init__(self, inventory = None):
+        if not inventory: 
+            inventory = []
+        self.inventory = inventory
+
+    # Adding item to inventory.
+    def add(self, item):
+        self.inventory.append(item)
+        return item
+    
+    # Removing item to inventory. Returning false if not in inventory.
+    def remove(self, item): 
+        if item not in self.inventory:
+            return False 
+        self.inventory.remove(item)
+        return item
+        
