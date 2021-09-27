c5d16bae15a520ba82fa61f0173967e7d76c79d6
Laurel S <ls60@protonmail.com>
2021-09-27 15:17:18 -0700

    Passes Wave 5. Established Decor, Clothing, and Electronics as children of Item class.

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,7 +1,18 @@
 from .item import Item
 
 class Clothing(Item):
-    def __init__(self, category = "Clothing"):
-        self.category = category
+    # new init method with new paramaters
+    def __init__(self, category = "", condition = None):
+    # def __init__(self, category, condition):
+        # call superclass' constructor
+        # super().__init__(category = "", condition = None)
+        super().__init__(category, condition)
+        self.category = "Clothing"
+
+    # def __init__(self, condition, category = ""):
+    #     Item.__init__(self, category, condition)
+    #     self.category = "clothing"
+    # #     self.category = category
+
     def __str__(self):
         return "The finest clothing you could wear."
