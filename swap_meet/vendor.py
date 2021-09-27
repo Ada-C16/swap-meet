149fd858759a8fc741d4f0418680142ba022a6a5
Laurel S <ls60@protonmail.com>
2021-09-27 14:30:08 -0700

    Passes Wave 3 (except for string override) and Wave 4.

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -26,17 +26,26 @@ class Vendor:
                 items_by_category.append(item)
         return items_by_category
 
-    def swap_items(self, vendor_name, item_out, item_in):
-        # remove my item from inv
-        self.remove(item_out)
-        # add item to other vendor's inv
-        vendor_name.add(item_out)
-        # remove new item from other vendor's inv
-        vendor_name.remove(item_in)
-        # add new item to my inv
-        self.add(item_in)
-        return True
+    def swap_items(self, other_vendor, item_out, item_in):
+        # are these two tests the most efficient route?
+        if item_out in self.inventory and item_in in other_vendor.inventory:
+            # remove my item from inv
+            self.remove(item_out)
+            # remove new item from other vendor's inv
+            other_vendor.remove(item_in)
+            # add new item to my inv
+            self.add(item_in)
+            # add item to other vendor's inv
+            other_vendor.add(item_out)
+            # return True
+            return True
+        # if items successfully added and removed, return True
+        return False
 
-
-
-        
+    def swap_first_item(self, other_vendor): 
+        if self.inventory and other_vendor.inventory:
+            item_in = other_vendor.inventory[0]
+            item_out = self.inventory[0]
+            self.swap_items(other_vendor, item_out, item_in)
+            return True
+        return False
