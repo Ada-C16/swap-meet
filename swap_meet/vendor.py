715bb805149463170e4971cb3c484f0906e3372c
karishma <karishmacarterjohnson@gmail.com>
2021-09-27 12:21:54 -0700

    waves 1 & 2 all tests passed

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,20 @@
+import sys
+
+from .item import Item
 class Vendor:
-    pass
+    def __init__(self, inventory=[]):
+        self.inventory = inventory
+    
+    def add(self, item):
+        self.inventory.append(item)
+        return item 
+    
+    def remove(self, item):
+        print(self.inventory)
+        if item in self.inventory:
+            self.inventory.remove(item)
+            return item 
+        else:
+            return False
+    def get_by_category(self, category):
+        return [item for item in self.inventory if item.category == Item(category=category).category]
