a8c4b69a004258e2fd4fb6e17fbd17b22eff34c4
Mac <mackenziespeyton@gmail.com>
2021-09-27 15:20:23 -0700

    Initialized item class. Added get_by_category method to vendor. passing wave 02.

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,18 +1,30 @@
+from .item import Item
+
 class Vendor:
     def __init__(self, inventory = None):
         if not inventory: 
             inventory = []
-        self.inventory = inventory
+        self.inventory = inventory # list of items
 
     # Adding item to inventory.
     def add(self, item):
         self.inventory.append(item)
         return item
     
-    # Removing item to inventory. Returning false if not in inventory.
+    # Removing item from inventory. 
+    # Returning false if not in inventory.
     def remove(self, item): 
         if item not in self.inventory:
             return False 
         self.inventory.remove(item)
         return item
+    
+    # Returning items within a given category
+    def get_by_category(self, category):
+        category_items_list = []
+
+        for item in self.inventory: 
+            if item.category == category:
+                category_items_list.append(item)
         
+        return category_items_list
