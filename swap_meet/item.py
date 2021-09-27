4379b9d49c2f2fc15532c0258e1b205be967442d
cecily-916 <cecily.marroquin@gmail.com>
2021-09-27 16:38:52 -0400

    commit first 5 waves

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,2 +1,23 @@
+
 class Item:
-    pass
+
+    def __init__(self, category = "", condition = 0):
+        self.category = category
+        self.condition = condition
+
+    def __str__(self):
+        return "Hello World!"
+
+    def condition_description(self):
+        if self.condition == 5:
+            return ("Mint!")
+        if self.condition == 4:
+            return ("It's great!")
+        if self.condition == 3:
+            return ("It's ok.")
+        if self.condition == 2:
+            return ("Not perfect but not broken.")
+        if self.condition == 1:
+            return ("Not the worst if you're a glass half full sort of person.")
+        if self.condition == 0:
+            return ("The worst no matter how you look at it.")
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,70 @@
+from typing import ItemsView
+from swap_meet.item import Item
+
 class Vendor:
-    pass
+    """
+    Attributes:: 
+    inventory: an empty list by default, 
+    optionally can pass in a list
+
+    Ins. methods::
+    add : takes in one item and adds it to the inventory
+    returns the item that was added
+
+    remove: takes in an item and removes it from inventory
+    returns the item removed
+
+    if no matching item in the inventory return false
+    """
+
+    def __init__(self, inventory = []):
+        self.inventory = inventory
+
+    def add(self, item):
+        self.inventory.append(item)
+        return item
+
+    def remove(self, item):
+        try:
+            self.inventory.remove(item)
+            return item
+        except ValueError:
+            return False
+
+    def get_by_category(self, category):
+        items_in_category = []
+
+        for item in self.inventory:
+            if item.category == category:
+                items_in_category.append(item)
+
+        return items_in_category
+
+    def swap_items(self, friend, my_item, their_item):
+
+        if not my_item in self.inventory or not their_item in friend.inventory:
+            return False
+
+        self.inventory.remove(my_item)
+        self.inventory.append(their_item)
+        friend.inventory.remove(their_item)
+        friend.inventory.append(my_item)
+        return True
+
+    
+    def swap_first_item(self, friend):
+        if len(self.inventory) == 0 or len(friend.inventory) == 0:
+            return False
+
+        my_first_item = self.inventory[0]
+        their_first_item = friend.inventory[0]
+
+        self.inventory.remove(my_first_item)
+        self.inventory.append(their_first_item)
+
+        friend.inventory.remove(their_first_item)
+        friend.inventory.append(my_first_item)
+
+        return True
+    
+    
