6c9a841e66bcae047a0acbe05c3afcb98827dbaa
Afina <afinawalton95@gmail.com>
2021-09-27 13:36:26 -0700

    Adds instance methods and attributes to Vendor class to pass wave 1 tests

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,20 @@
 class Vendor:
-    pass
+    
+    def __init__(self, inventory=None): # Set a default value
+        if inventory is None: # Default value is falsy, so set to empty list
+            self.inventory = []
+        else:
+            self.inventory = inventory # If inventory is given, set it equal to inventory attribute
+
+    def add(self, item):
+        self.inventory.append(item)
+        return item
+
+    def remove(self, item):
+        if item in self.inventory:
+            self.inventory.remove(item)
+        else:
+            return False
+        return item
+
+    
