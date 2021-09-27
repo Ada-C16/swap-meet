78dc0e557a3105ba6a512bab11d2f3acfd70db8c
Alie Ibarra <alieibarra@gmail.com>
2021-09-27 14:01:28 -0700

    Pass all Wave 1 Tests

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,45 @@
+#WAVE 01
+# [X] For Tests 1-2 
+#     [X] Each Vendor will have an attribute named 
+#         inventory, which is an empty list by default
+#     [X] When we create initialize an instance of Vendor, 
+#         we can optionally pass in a list with the keyword 
+#         argument inventory
+# [ ] For Tests 3-5
+#     [X] Every instance of Vendor has an instance method 
+#         named add, which takes in one item 
+#     [X] This method adds the item to the inventory 
+#     [X] This method returns the item that was added
+#     [X] Similarly, every instance of Vendor has an instance 
+#         method named remove, which takes in one item
+#     [ ] This method removes the matching item from the inventory
+#     [ ] This method returns the item that was removed
+#     [ ] If there is no matching item in the inventory, the method 
+#         should return False
+
+
 class Vendor:
-    pass
+    def __init__ (self, inventory = None):
+        if not inventory:
+            inventory = []
+        self.inventory = inventory
+    
+    def add(self, add_item):
+        #add item from inventory list
+        self.inventory.append(add_item)
+        #return added item
+        return add_item
+
+    def remove(self, remove_item):
+        #if remove item in inventory list
+        if remove_item in self.inventory:
+            #remove item from inventory list
+            self.inventory.remove(remove_item)
+            #return removed item
+            return remove_item
+        #otherwise return False
+        else: 
+            return False
+
+
+
