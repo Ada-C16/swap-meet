a8d484c0a988ab4d1962e5cf87ff39c1ce23a78e
Reid Maute <reidhowdy@gmail.com>
2021-09-28 15:13:05 -0700

    complete through wave 4

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -3,5 +3,12 @@ from swap_meet.vendor import Vendor
 class Item:
     def __init__(self, category = ""):
         self.category = category
+
+    def __str__(self):
+        return "Hello World!"
+  
+
+
+
     
-    
+
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,6 +1,9 @@
 class Vendor:
-    def __init__(self, inventory = []):
-        self.inventory = inventory
+    def __init__(self, inventory = None):
+        if inventory is None:
+            self.inventory = []
+        else:
+            self.inventory = inventory
     
     def add(self, item_to_add):
         self.inventory.append(item_to_add)
@@ -12,5 +15,41 @@ class Vendor:
         #or maybe throw an error instead here?
         self.inventory.remove(item_to_remove)
         return item_to_remove
+
+    def get_by_category(self, category):
+        category_matches = []
+        for item in self.inventory:
+            if item.category == category:
+                category_matches.append(item)
+        return category_matches
+
+    def swap_items(self, other_vendor, selfs_item, others_item):
+        if selfs_item in self.inventory and others_item in other_vendor.inventory:
+            self.inventory.remove(selfs_item)
+            self.inventory.append(others_item)
+
+            other_vendor.inventory.remove(others_item)
+            other_vendor.inventory.append(selfs_item)
+
+            return True
+            
+        else:
+            return False
+    
+    def swap_first_item(self, other_vendor):
+        if (len(self.inventory) == 0) or (len(other_vendor.inventory) == 0):
+            return False
+        else:
+            selfs_item = self.inventory.pop(0)
+            others_item = other_vendor.inventory.pop(0)
+
+            self.inventory.insert(0, others_item)
+            other_vendor.inventory.insert(0, selfs_item)
+            return True
+
+    
+
+    
+        
 
     
