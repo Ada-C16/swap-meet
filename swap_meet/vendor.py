de117de839264c7574c212596929a31da3211a34
Citlalli I Zarate <37793116+MiffyBruna@users.noreply.github.com>
2021-09-28 15:44:41 -0700

    All the tests from test_wave_01 pass ／(=๏ x n=)"

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,18 @@
 class Vendor:
+    def __init__(self, inventory=None):
+        if inventory == None:
+            inventory = []
+        self.inventory = inventory
+
+    def add(self, item):
+        self.inventory.append(item)
+        return item
+
+    def remove(self, item_to_remove):
+        if item_to_remove in self.inventory:
+            self.inventory.remove(item_to_remove)
+            return item_to_remove
+        else:
+            return False
+            
     pass
