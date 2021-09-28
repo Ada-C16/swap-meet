2b34b21dc9802c4014a9894d91a56def95d6a412
ayaka <ayakaob@gmail.com>
2021-09-27 20:45:44 -0700

    pass all tests of wave 3

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -15,3 +15,12 @@ class Vendor:
 
     def get_by_category(self, category):
         return [item for item in self.inventory if item.category == category]
+    
+    def swap_items(self, other_vendor, item_to_give, item_to_receive):
+        if item_to_give not in self.inventory or item_to_receive not in other_vendor.inventory:
+            return False
+        self.inventory.remove(item_to_give)
+        other_vendor.inventory.append(item_to_give)
+        self.inventory.append(item_to_receive)
+        other_vendor.inventory.remove(item_to_receive)
+        return True
