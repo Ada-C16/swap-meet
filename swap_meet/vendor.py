a30f3bc97a83bc626cc1f3bbf737bdaaeb417803
ayaka <ayakaob@gmail.com>
2021-09-27 22:46:06 -0700

    pass all tests of wave 6

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -33,3 +33,24 @@ class Vendor:
         item_to_receive = other_vendor.inventory.pop(0)
         self.inventory.append(item_to_receive)
         return True
+
+
+    def get_best_by_category(self, category):
+        best_item = None
+        best_condition = 0
+        for item in self.inventory:
+            if item.category == category and item.condition > best_condition:
+                best_item = item
+                best_condition = item.condition
+        return best_item
+
+    def swap_best_by_category(self, other, my_priority, their_priority):
+        item_to_give = self.get_best_by_category(their_priority)
+        item_to_receive = other.get_best_by_category(my_priority)
+        if not item_to_give or not item_to_receive:
+            return False
+        self.remove(item_to_give)
+        other.add(item_to_give)
+        self.add(item_to_receive)
+        other.remove(item_to_receive)
+        return True
