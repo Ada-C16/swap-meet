d6a7e2fb99caec85f793314efa7b040f4bb1858a
Jesse <jesseannepope@gmail.com>
2021-09-27 20:50:25 -0700

    waves one and two

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,28 @@
+from swap_meet.item import Item
+
 class Vendor:
-    pass
+    def __init__(self, inventory = []):
+        # if not inventory:
+        #     inventory = []
+        self.inventory = inventory
+        self.item = Item()
+
+    def add(self, item):
+        self.inventory.append(item)
+        return item
+    
+    def remove(self, item):
+        if item in self.inventory:
+            self.inventory.remove(item)
+            return item
+        else:
+            return False
+
+    def get_by_category(self, category):
+        items = []
+        for item in self.inventory:
+            if item.category == category:
+                items.append(item)
+        return items
+
+
