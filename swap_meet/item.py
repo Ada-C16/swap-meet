1c6a033f8a076bb2edfb49d36123846e17c77383
sarahstandish <z.sisyphusrocks@gmail.com>
2021-09-28 16:57:40 -0700

    condition description returns string

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -3,12 +3,12 @@ class Item:
     def __init__(self, condition=0.0, category=""):
 
         self.category = category.title()
-        self.condition = float(condition)
+        self.condition = condition
 
     def __str__(self):
 
         return "Hello World!"
 
     def condition_description(self):
-        return self.condition
+        return str(self.condition)
     
