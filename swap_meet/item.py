65f60caceda0c417b5b2490900c5b72af3c8ec8b
Karina <infernormal@gmail.com>
2021-09-28 13:12:10 -0700

    wave 5 pass

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,8 +1,23 @@
 class Item:
-    def __init__(self,category = None):
+    def __init__(self,category = None, condition = 0):
         if not category:
             category = ""
         self.category = category
+        self.condition = condition
 
     def __str__(self):
         return ("Hello World!")
+
+    def condition_description(self):
+        if self.condition ==0:
+            return ("No rating")
+        elif self.condition ==1:
+            return("Very heavily used")
+        elif self.condition ==2:
+            return("Heavily used")
+        elif self.condition ==3:
+            return("Used")
+        elif self.condition ==4:
+            return("In good condition")
+        elif self.condition ==5:
+            return("Like new")
