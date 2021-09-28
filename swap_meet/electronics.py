bb2f1b6b040b939768d875c23e871e9047d61419
sarahstandish <z.sisyphusrocks@gmail.com>
2021-09-28 14:33:31 -0700

    add init and string methods

--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -1,2 +1,10 @@
-class Electronics:
-    pass
+from swap_meet.item import Item
+
+class Electronics(Item):
+    
+    def __init__(self):
+        super().__init__(category="Electronics")
+
+    def __str__(self) -> str:
+        return "A gadget full of buttons and secrets."
+
