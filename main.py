a24792e9323eb56413131dd1a22aec3dd2078297
Areeba Rashid <areeba.r.qureshi@gmail.com>
2021-09-28 14:29:28 -0700

    the commit message thing won't go away so i'm typing this

---
+++ main.py
@@ -0,0 +1,2 @@
+from swap_meet.item import Item
+from swap_meet.vendor import Vendor
--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,6 +1,25 @@
 class Item:
-    def __init__(self, category = ""):
+    def __init__(self, category = "", condition = 0):
         self.category = category
+        self.condition = condition
 
     def __str__(self):
         return "Hello World!" 
+
+    def condition_description(self):
+        description = "condition information unavailable"
+        
+        if self.condition == 0:
+            description = "New with tags! Either a gift or they missed the return date."
+        elif self.condition == 1:
+            description = "New without tags. They changed their mind right away."
+        elif self.condition == 2:
+            description = "Excellent condition. They bought it, used it and then immediately found something better..."
+        elif self.condition == 3:
+            description = "Gently used. Previously loved, but little sign of wear."
+        elif self.condition == 4:
+            description = "Used. Some signs of wear. How much do you really want this and can you not find it elsewhere?"
+        elif self.condition == 5:
+            description = "Worn. Heavily used. Consider passing."
+
+        return description
