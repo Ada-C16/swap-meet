a16ab32ad9a1459907a1e5d564e273ace123d366
sarahstandish <z.sisyphusrocks@gmail.com>
2021-09-28 13:42:06 -0700

    add condition attribute and condition_description method

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,2 +1,14 @@
 class Item:
-    pass
+    
+    def __init__(self, category="", condition=0.0):
+
+        self.category = category
+        self.condition = condition
+
+    def __str__(self):
+
+        return "Hello World!"
+
+    def contidition_description(self):
+        return self.condition
+    
