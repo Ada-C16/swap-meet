3d68fe239590e9e8c1e5cc638aa71cfb86200cce
Kristel Jabbusch <kaj12@my.fsu.edu>
2021-09-28 09:19:47 -0700

    item imported to vendor, second wave passes

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,5 +1,25 @@
+from .item import Item
+
 class Vendor:
     def __init__(self, inventory = None):
         if not inventory:
             inventory = []
         self.inventory = inventory
+
+    def add(self, item):
+        self.inventory.append(item)
+        return item
+
+    def remove(self, item):
+        if item not in self.inventory:
+            return False
+        self.inventory.remove(item)
+        return item
+
+    def get_by_category(self, category):
+        items = []
+
+        for item in self.inventory:
+            if item.category == category:
+                items.append(item)
+        return items
