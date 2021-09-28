77d462d7b0c7ba04bca7b0237925ab5c0b61b899
tt-ht <ttht@protonmail.com>
2021-09-27 18:39:12 -0700

    wave 1 and wave 2 tests pass; Vendor and Item classes defined

---
+++ main.py
@@ -0,0 +1,9 @@
+from swap_meet.clothing import Clothing
+from swap_meet.decor import Decor
+from swap_meet.electronics import Electronics
+from swap_meet.item import Item
+from swap_meet.vendor import Vendor
+
+
+clothing = Clothing()
+decor = Decor()
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,27 @@
+from swap_meet.item import Item
+
 class Vendor:
-    pass
+    def __init__(self, inventory = None):
+        if not inventory:
+            inventory = []
+        
+        self.inventory = inventory
+
+    def add(self, item):
+        self.inventory.append(item)
+        return item
+    
+    def remove(self, item):
+        if item in self.inventory:
+            self.inventory.remove(item)
+            return item
+        else:
+            return False
+
+    def get_by_category(self, category):
+        items_by_category = []
+        for item in self.inventory:
+            if item.category == category:
+                items_by_category.append(item)
+
+        return items_by_category
