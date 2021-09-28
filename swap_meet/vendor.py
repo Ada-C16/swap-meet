fa7d41506eb9dd2ea6363e48c4a1dd3d747e394e
Mac <mackenziespeyton@gmail.com>
2021-09-28 13:26:57 -0700

    Added function swap_items that swaps item between two vendors. Updated doc strings. Passing test waves 1-3.

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -4,23 +4,29 @@ class Vendor:
     def __init__(self, inventory = None):
         if not inventory: 
             inventory = []
-        self.inventory = inventory # list of items
+        self.inventory = inventory # List of item objects
 
-    # Adding item to inventory.
     def add(self, item):
+        """
+        Adding item to inventory. 
+        """
         self.inventory.append(item)
         return item
     
-    # Removing item from inventory. 
-    # Returning false if not in inventory.
     def remove(self, item): 
+        """
+        Removing item from inventory. 
+        Returning false if not in inventory.
+        """
         if item not in self.inventory:
             return False 
         self.inventory.remove(item)
         return item
     
-    # Returning items within a given category
     def get_by_category(self, category):
+        """
+        Returning items within a given category
+        """
         category_items_list = []
 
         for item in self.inventory: 
@@ -28,3 +34,23 @@ class Vendor:
                 category_items_list.append(item)
         
         return category_items_list
+
+    def swap_items(self, friend, my_item, their_item):
+        """
+        Swapping items between two vendors. 
+        Returns false if items not in one or both inventories. 
+        """
+        if my_item not in self.inventory or \
+        their_item not in friend.inventory: 
+            return False
+
+        # Swapping my_item
+        self.inventory.remove(my_item)
+        friend.inventory.append(my_item)
+
+        # Swapping their_item
+        self.inventory.append(their_item)
+        friend.inventory.remove(their_item)
+
+        return True
+
