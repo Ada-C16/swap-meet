6db6fd6e69a87a45fca825d58d0108c6b34f4972
Jessica <jessica.w.roberts@gmail.com>
2021-09-28 11:46:09 -0700

    Add code to pass test wave 1

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,16 @@
 class Vendor:
-    pass
+    def __init__(self, inventory = None):
+        if inventory == None:
+            inventory = []
+        self.inventory = inventory
+
+    def add(self, item):
+        self.inventory.append(item)
+        return item
+
+    def remove(self, item):
+        index_list = [i for (i, value) in enumerate(self.inventory) if value == item]
+        if len(index_list) == 0:
+            return False
+        else:
+            return self.inventory.pop(index_list[0])
