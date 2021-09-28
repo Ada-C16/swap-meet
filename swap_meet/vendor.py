576798bcc1f53a911f9f6e3b6a1b694b02abf22c
Karina <infernormal@gmail.com>
2021-09-27 21:55:59 -0700

    item class and wave 2,3,4

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -14,3 +14,26 @@ class Vendor:
         else:
             self.inventory.remove(item)
         return item
+
+    def get_by_category(self,category):
+        items_in_category = []
+        for item in self.inventory:
+            if item.category == category:
+                items_in_category.append(item)
+        return items_in_category
+
+    def swap_items(self,vendor,my_item,their_item):
+        if my_item not in self.inventory or their_item not in vendor.inventory:
+            return False
+        self.remove(my_item)
+        vendor.add(my_item)
+        vendor.remove(their_item)
+        self.add(their_item)
+        return True
+
+    def swap_first_item(self, vendor):
+        if not self.inventory or not vendor.inventory:
+            return False
+        self.swap_items(vendor,self.inventory[0], vendor.inventory[0])
+        return True
+
