98ba4bd776544d9c4172a9578a20cbd804ba36f6
Tirhas Gebreyohannes <tirhastse@gmail.com>
2021-09-28 14:53:20 -0700

    test_wave_03 passed

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 from swap_meet.vendor import Vendor
 from swap_meet.item import Item
 
-item_a = Item(category="clothing")
-item_b = Item(category="electronics")
-item_c = Item(category="clothing")
+# item_a = Item(category="clothing")
+# item_b = Item(category="electronics")
+# item_c = Item(category="clothing")
 
-vendor_1 = Vendor(inventory = [item_b,item_b])
+# vendor_1 = Vendor(inventory = [item_b,item_b])
+item_a = Item(category="clothing")
+item_b = Item(category="clothing")
+item_c = Item(category="clothing")
+fatimah = Vendor(
+        inventory=[item_a, item_b, item_c]
+    )
+
+item_d = Item(category="electronics")
+item_e = Item(category="decor")
+jolie = Vendor(
+        inventory=[item_d, item_e]
+    )
+
+result = fatimah.swap_items(jolie, item_b, item_d)
+print(len(result))
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -20,3 +20,14 @@ class Vendor:
             if item.category == catagory_name:
                 mylist.append(item)
         return mylist
+    
+    def swap_items(self,friend,my_item,their_item ):
+        if my_item not in self.inventory  or their_item not in friend.inventory:
+            return False
+        self.remove(my_item)
+        friend.add(my_item)
+        friend.remove(their_item)
+        self.add(their_item)
+        return True
+        
+        
