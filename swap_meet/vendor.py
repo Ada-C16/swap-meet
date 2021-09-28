355d08cd52d581edc6651668122643a2f6bc62ab
Khandice <s.khandice@gmail.com>
2021-09-27 17:35:39 -0700

    Updated Vendor class with one attribute of inventory list and add() and remove() methods. Wave_01 tests pass

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,15 @@
 class Vendor:
-    pass
+    def __init__(self, inventory=None):
+        if not inventory:
+            inventory = []
+        self.inventory = inventory
+    
+    def add(self, item_add):
+        self.inventory.append(item_add)
+        return item_add
+
+    def remove(self, item_remove):
+        if item_remove not in self.inventory:
+            return False
+        self.inventory.remove(item_remove)
+        return item_remove
