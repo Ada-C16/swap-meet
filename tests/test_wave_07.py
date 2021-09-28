b7cf85ef32efd17d8b65605a8d9f97825a8dc758
Kristel Jabbusch <kaj12@my.fsu.edu>
2021-09-28 13:33:33 -0700

    all tests for age, wave 7 passes

--- tests/test_wave_07.py
+++ tests/test_wave_07.py
@@ -41,3 +41,102 @@ def test_get_by_newest_with_duplicates():
     assert newest_item.category == "Clothing"
     assert newest_item.age == pytest.approx(2.0)
 
+def test_swap_best_by_newest():
+    item_a = Decor(age=2.0)
+    item_b = Electronics(age=4.0)
+    item_c = Decor(age=4.0)
+    tai = Vendor(
+        inventory=[item_a, item_b, item_c]
+    )
+
+    item_d = Clothing(age=2.0)
+    item_e = Decor(age=4.0)
+    item_f = Clothing(age=4.0)
+    jesse = Vendor(
+        inventory=[item_d, item_e, item_f]
+    )
+
+    result = tai.swap_by_newest(other=jesse)
+
+    assert result
+    assert len(tai.inventory) == 3
+    assert len(jesse.inventory) == 3
+    assert item_a not in tai.inventory
+    assert item_b in tai.inventory
+    assert item_c in tai.inventory
+    assert item_d in tai.inventory
+    assert item_d not in jesse.inventory
+    assert item_e in jesse.inventory
+    assert item_f in jesse.inventory
+    assert item_a in jesse.inventory
+
+def test_swap_best_by_newest_reordered():
+    item_a = Decor(age=2.0)
+    item_b = Electronics(age=4.0)
+    item_c = Decor(age=4.0)
+    tai = Vendor(
+        inventory=[item_c, item_b, item_a]
+    )
+
+    item_d = Clothing(age=2.0)
+    item_e = Decor(age=4.0)
+    item_f = Clothing(age=4.0)
+    jesse = Vendor(
+        inventory=[item_f, item_e, item_d]
+    )
+
+    result = tai.swap_by_newest(other=jesse)
+
+    assert result
+    assert len(tai.inventory) == 3
+    assert len(jesse.inventory) == 3
+    assert item_a not in tai.inventory
+    assert item_b in tai.inventory
+    assert item_d in tai.inventory
+    assert item_c in tai.inventory
+    assert item_d not in jesse.inventory
+    assert item_e in jesse.inventory
+    assert item_f in jesse.inventory
+    assert item_a in jesse.inventory
+
+def test_swap_best_by_newest_no_inventory_is_false():
+    tai = Vendor(
+        inventory=[]
+    )
+
+    item_a = Clothing(age=2.0)
+    item_b = Decor(age=4.0)
+    item_c = Clothing(age=4.0)
+    jesse = Vendor(
+        inventory=[item_a, item_b, item_c]
+    )
+
+    result = tai.swap_by_newest(other=jesse)
+
+    assert not result
+    assert len(tai.inventory) == 0
+    assert len(jesse.inventory) == 3
+    assert item_a in jesse.inventory
+    assert item_b in jesse.inventory
+    assert item_c in jesse.inventory
+
+def test_swap_best_by_newest_no_other_inventory_is_false():
+    item_a = Clothing(age=2.0)
+    item_b = Decor(age=4.0)
+    item_c = Clothing(age=4.0)
+    tai = Vendor(
+        inventory=[item_a, item_b, item_c]
+    )
+
+    jesse = Vendor(
+        inventory=[]
+    )
+
+    result = tai.swap_by_newest(other=jesse)
+
+    assert not result
+    assert len(tai.inventory) == 3
+    assert len(jesse.inventory) == 0
+    assert item_a in tai.inventory
+    assert item_b in tai.inventory
+    assert item_c in tai.inventory
