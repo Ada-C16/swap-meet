cf38ea04bbe1310e2eaff714ce75689d16b1b5cb
Kristel Jabbusch <kaj12@my.fsu.edu>
2021-09-28 12:18:59 -0700

    fixed item class no need to import, is parent

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,11 +1,5 @@
-from .clothing import Clothing
-from .decor import Decor
-from .electronics
-
 class Item:
     def __init__(self, category = None, condition = None):
-        if not category:
-            category = ""
         self.category = category
         self.condition = condition
 
