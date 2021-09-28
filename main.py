460b89fbd401e437ed2fcdb824e1373c95a1286c
sarahstandish <z.sisyphusrocks@gmail.com>
2021-09-28 13:51:11 -0700

    test vendor and item creation

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
 from swap_meet.vendor import Vendor 
 from swap_meet.item import Item
+
+radio = Item("electronics")
+shirt = Item("clothing")
+pan = Item("cookware")
+shoes = Item("clothing")
+earbuds = Item("electronics")
+
+v1 = Vendor([radio, shirt, pan, shoes, earbuds])
+
+print(v1.get_by_category("clothing"))
