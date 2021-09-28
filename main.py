0c35ec290c497cc78a412309fbb03a00c7e0be4b
sarahstandish <z.sisyphusrocks@gmail.com>
2021-09-28 16:58:40 -0700

    test child classes of Item

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
 from swap_meet.vendor import Vendor 
 from swap_meet.item import Item
+from swap_meet.clothing import Clothing
+from swap_meet.decor import Decor
+from swap_meet.electronics import Electronics
 
 radio = Item("electronics")
-shirt = Item("clothing")
+shirt = Clothing()
 pan = Item("cookware")
-shoes = Item("clothing")
+shoes = Clothing()
 earbuds = Item("electronics")
 
 v1 = Vendor([radio, shirt, pan, shoes, earbuds])
 
-print(v1.get_by_category("clothing"))
+v1.get_by_category("clothing")
+
+pants = Clothing(condition=3.5)
+candlestick = Decor(condition=3.5)
+phone = Electronics(condition=3.5)
+
+items = [
+    pants,
+    candlestick,
+    phone
+]
+for item in items:
+    print(item.condition)
