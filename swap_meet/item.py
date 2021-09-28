32f27c596e86c0a556ce80084595d12849329b1f
gabi <annagabsoares@gmail.com>
2021-09-28 13:39:58 -0700

    Implement the subclasses ->  Clothing() Decor() Electronics()   - Implement the superclass Item() and pass wave5 tests

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,4 +1,16 @@
 class Item:
-    def __init__(self, category= str()):
+    def __init__(self, category= str(), condition =0 ):
         self.category = category
-
+        self.condition = condition
+        
+    def __str__(self):
+        return "Hello World!"
+    
+    def condition_description(self):
+        value = self.condition
+        if value >4:
+            return 'Great condition'
+        if value > 3:
+            return "Used but in good conditon"
+        if value < 3:
+            return "Heavily used"
