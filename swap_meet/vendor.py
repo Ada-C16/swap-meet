d4e2faae105239d0fb14ef93fc320df41349e40a
Arianna Cabebe <aricabebe@gmail.com>
2021-09-28 13:02:23 -0700

    Added Vendor class, passes test wave 1

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,17 @@
 class Vendor:
-    pass
+    
+    def __init__(self, inventory=[]): 
+        self.inventory = inventory
+
+    def add(self, new_item):
+        """ Adds argument passed into new_item to Vendor's inventory list, and returns the new item. """
+        self.inventory.append(new_item)
+        return new_item
+    
+    def remove(self, item_to_remove):
+        """ Removes argument passed into item_to_remove from Vendor's inventory list, and returns the removed item. """
+        if item_to_remove not in self.inventory:
+            return False
+        self.inventory.remove(item_to_remove)
+        removed_item = item_to_remove
+        return removed_item
