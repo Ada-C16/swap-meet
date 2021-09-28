42514622e058ef14cd1a991cf022855d0f922c0f
sarahstandish <z.sisyphusrocks@gmail.com>
2021-09-28 15:18:47 -0700

    fix error in passing initializing attributes via super class

--- swap_meet/decor.py
+++ swap_meet/decor.py
@@ -2,8 +2,8 @@ from swap_meet.item import Item
 
 class Decor(Item):
     
-    def __init__(self):
-        super().__init__(category="Decor")
+    def __init__(self, condition=0.0):
+        super().__init__(condition=condition, category="Decor")
 
     def __str__(self):
         return "Something to decorate your space."
--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -2,8 +2,8 @@ from swap_meet.item import Item
 
 class Electronics(Item):
     
-    def __init__(self):
-        super().__init__(category="Electronics")
+    def __init__(self, condition=0.0):
+        super().__init__(condition=condition, category="Electronics")
 
     def __str__(self) -> str:
         return "A gadget full of buttons and secrets."
