e747372950eb93708f5b9727334ff65c577590ba
Karina <infernormal@gmail.com>
2021-09-28 14:28:51 -0700

    removed unnecessary function from 3 files, added wave 6 pass

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -37,3 +37,24 @@ class Vendor:
         self.swap_items(vendor,self.inventory[0], vendor.inventory[0])
         return True
 
+
+    def get_best_by_category(self,category):
+        category_list = self.get_by_category(category)
+        if not category_list:
+            return None
+        else:
+            return max(category_list, key=lambda item:item.condition)
+
+
+    def swap_best_by_category(self, other, my_priority, their_priority):
+        my_best_item = self.get_best_by_category(their_priority)
+        their_best_item = other.get_best_by_category(my_priority)
+        if my_best_item == None:
+            return False
+        elif their_best_item == None:
+            return False
+        else:
+            self.swap_items(other,my_best_item, their_best_item)
+            return True
+
+            
