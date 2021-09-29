157d76c9998a39e55ec119d9a62c4134595c2268
Rhyannon <rhyannonjoy@gmail.com>
2021-09-29 15:02:25 -0600

    Passes Wave 5 with super()

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,7 +1,7 @@
 class Item:
-    def __init__(self, category="", condition=None):
+    def __init__(self, category=None, condition=0):
         self.category = category if category is not None else ""
-        self.category = category
+        self.condition = condition 
 
     def __str__(self):
         return 'Hello World!'
