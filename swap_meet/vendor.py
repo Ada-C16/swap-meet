3256e658a9b7a960f08df304cf5ed5fcbfa4a726
Ainur Dzhaianbaeva <adjayanbaeva@gmail.com>
2021-09-28 23:58:12 -0700

    add swap_items() Vendor instance method

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,3 +1,5 @@
+from .item import Item
+
 class Vendor:
     def __init__(self, inventory = None):
         if not inventory:
@@ -14,3 +16,26 @@ class Vendor:
             return item
         else:
             return False
+
+    # def get_by_category(self, string_category):
+    #     list_of_items = []
+    #     for i in range(len(self.inventory)):
+    #         if self.inventory[i] == string_category:
+
+    #             list_of_items.append(Item(self.inventory[i]))
+                
+    #     return list_of_items
+
+    def swap_items(self, friends_inventory, my_item, their_item):
+        if my_item in self.inventory and their_item in friends_inventory.inventory:
+            self.add(their_item)
+            friends_inventory.add(my_item)
+            self.remove(my_item)
+            friends_inventory.remove(their_item)
+            return True
+        else:
+            return False
+            
+          
+      
+
