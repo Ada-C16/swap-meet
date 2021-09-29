66dc7465baa45425c3d0cb55a7c6ed79ca3d9a73
Faith <fkauwe@gmail.com>
2021-09-28 23:02:03 -0700

    all tests passing through wave5

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,7 +1,21 @@
 class Item:
-    def __init__(self, category =''):
+    def __init__(self, category ='', condition = 0):
         self.category = category
+        self.condition = condition
     def __str__(self):
         return 'Hello World!'
+
+    def condition_description(self):
+        if self.condition > 0 and self.condition <1.1:
+            return 'Very Bad Condition'
+        elif self.condition >= 1.1 and self.condition <2.1:
+            return 'Pretty Bad Condition'
+        elif self.condition >= 2.1 and self.condition <3.1:
+            return 'Acceptable Condition'
+        elif self.condition >= 3.1 and self.condition <4.1:
+            return 'Pretty Good Condition'
+        elif self.condition >= 4.1 and self.condition <5.1:
+            return 'Very Good Condition'
+
 
     
