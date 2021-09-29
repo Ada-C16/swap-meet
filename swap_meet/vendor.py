3823a18ab75c78c310520c847404676ec7a4482f
Rebecca Zuick <rebecca.zuick@gmail.com>
2021-09-28 23:35:48 -0500

    completed swap_items function. passed all tests in wave 3

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,5 +1,6 @@
 from swap_meet.item import Item
 
+
 class Vendor:
     """
     This class has one attribute, inventory, which is an empty list by default. 
@@ -8,8 +9,10 @@ class Vendor:
     Both methods will take in one argument, item. The add method will append the item to 
     the list and return item. Remove will remove the item from list and return item.
     """
-    def __init__(self, inventory = []):
-        self.inventory = inventory
+
+    def __init__(self, inventory=None):
+
+        self.inventory = inventory if inventory is not None else []
         self.category_list = []
 
     def add(self, item):
@@ -20,10 +23,30 @@ class Vendor:
         if item in self.inventory:
             self.inventory.remove(item)
             return item
-        
+
     def get_by_category(self, category):
-        # return list of item with 
+        # return list of item with
         for item in self.inventory:
             if item.category == category:
                 self.category_list.append(item)
         return self.category_list
+
+    def swap_items(self, friend_vendor, my_item, their_item):
+        """
+        Takes in three arguments: friend_vendor, my_item, and their_item.
+        friend_vendor is an instance of Vendor class, my_item and their_item
+        are instances of Item class. If my_item not in inventory or their_item
+        not in friend_vendor inventory, return False. Otherwise, remove the item from
+        the original inventory and add it to the others' inventory.
+        """
+
+        if their_item not in friend_vendor.inventory or \
+                my_item not in self.inventory:
+            return False
+
+        friend_vendor.add(my_item)
+        self.add(their_item)
+        friend_vendor.remove(their_item)
+        self.remove(my_item)
+
+        return True
