bb6a96d9e8fb624baabb2b9fe34e8af5e8bab48f
Monica Cruz <mcatcruz@gmail.com>
2021-09-29 00:42:25 -0400

    Wave 3. Started swap_items instance method. Passing all but one test.

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -25,3 +25,22 @@ class Vendor:
             else:
                 continue
         return categories_present_list
+
+    def swap_items(self, friend, my_item, their_item):
+        if my_item in self.inventory and their_item in friend.inventory:
+            # Remove my_item from MY inventory. 
+            # Add removed item to THEIR inventory.
+            my_removed_item = self.remove(my_item)
+            friend_removed_item = friend.remove(their_item)
+
+            # their_item:
+            # Remove an item from THEIR inventory.
+            # Add removed item to MY inventory.
+            self.add(friend_removed_item)
+            friend.add(my_removed_item)
+        else:
+            # return False if:
+            # my_item is not in self.inventory
+            # their_item is not in friend's inventory
+            return False
+        return True
