19e88495f35a4de257a57432698312976af830c7
jacy <jacyyang04@gmail.com>
2021-09-29 09:57:04 -0700

    Make decor, clothing and electronics a child class of item

--- main.py
+++ main.py
@@ -1,20 +1,10 @@
 from swap_meet.vendor import Vendor
 from swap_meet.item import Item
+from swap_meet.clothing import Clothing
+from swap_meet.decor import Decor
 
-item_a = Item(category="clothing")
-item_b = Item(category="clothing")
-item_c = Item(category="clothing")
-fatimah = Vendor(
-    inventory=[item_a, item_b, item_c]
-)
+cloth = Decor()
 
-item_d = Item(category="electronics")
-item_e = Item(category="decor")
-jolie = Vendor(
-    inventory=[item_d, item_e]
-)
-
-result = fatimah.swap_first_item(jolie)
-
+test = cloth.category
 #print(fatimah)
-print(result)
+print(test)
