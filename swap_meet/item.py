3ab226e6c7d087cced3cd42eb4c865ea7d59f662
Alma <abecerrilsalas@gmail.com>
2021-09-29 14:13:08 -0700

    changed the import in item

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -2,9 +2,27 @@
 
 
 class Item:#might have to chage and add the conditional
-    def __init__(self, category=""):
+    def __init__(self, category="", condition = 0):
 
         self.category=category
+        self.condition=condition
     
     def __str__(self):
         return "Hello World!"
+
+    def condition_description(self):
+        # return f"{self.condition}"
+        if self.condition > 0 and self.condition < 1: 
+            return "hello"
+        elif self.condition ==1:
+            return "sup"
+        elif self.condition==2:
+            return "what up"
+        elif self.condition ==3:
+            return "hola"
+        elif self.condition ==4:
+            return "whats good"
+        elif self.condition ==5:
+            return "yo"
+        else:
+            return "yo invalid"
