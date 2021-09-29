3fd9eb4260f039cd71fffb46e9a9407ee7e2ba1b
Mariah Rosman <mrosman7@gmail.com>
2021-09-29 11:44:53 -0700

    refacotrs Decord class constructor and adds super

--- swap_meet/decor.py
+++ swap_meet/decor.py
@@ -1,8 +1,8 @@
-from swap_meet.item import Item
+from .item import Item
 
 class Decor(Item):
-    def __init__(self):
-        super().__init__(self, condition=0)
+    def __init__(self, condition=0):
+        super().__init__(condition = condition)
         self.category = "Decor"
 
     def __str__(self):
