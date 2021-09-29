0881f67407a926a1aebf5148f05089c779a996a9
Arianna Cabebe <aricabebe@gmail.com>
2021-09-28 19:02:20 -0700

    made Item parent class to Electonics, Clothing, and Decor. added condition_description()

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,7 +1,11 @@
 class Item:
     
-    def __init__(self, category=''):
+    def __init__(self, category='', condition=0):
         self.category = category
+        self.condition = condition
 
     def __str__(self):
         return "Hello World!"
+    
+    def condition_description(self):
+        return f"The condition of this item is {self.condition}"
