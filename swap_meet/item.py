13e2c2d282a1fefac5034610d6171ff9346f94dd
Kristel Jabbusch <kaj12@my.fsu.edu>
2021-09-28 23:31:16 -0700

    lol unique condition descriptions

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -8,4 +8,15 @@ class Item:
         return "Hello World!"
 
     def condition_description(self):
-        return f"This item's condition is {self.condition}"
+        if 0 <= self.condition and self.condition < 1:
+            return "Cast this into Mordor. Immediately."
+        elif 1 <= self.condition and self.condition < 2:
+            return "An item only a mother could love."
+        elif 2 <= self.condition and  self.condition < 3:
+            return "Hopefully we can repurpose this."
+        elif 3 <= self.condition and self.condition < 4:
+            return "Nothing to write home about, but somewhat useful."
+        elif 4 <= self.condition and self.condition < 5:
+            return "Butter my rump and call me toast, this is fantastic."
+        else:
+            return "*chef's kiss*"
