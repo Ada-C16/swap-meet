cdf925ab29424a448a12a0d0f6738b5e10aef840
Edmine <edmine.de@gmail.com>
2021-09-28 22:13:43 -0400

    "Wave 1, Wave 2, and Wave 3 completed"

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,41 @@
+#Wave 1
+# from item import Item
 class Vendor:
-    pass
+    def __init__(self, inventory=[]):
+        self.inventory = inventory
+    def add(self,item):
+        self.inventory.append(item)
+        return item 
+    def remove(self,item):
+        if item not in self.inventory:
+            return False
+
+        self.inventory.remove(item)
+        return item 
+    
+#Wave 2
+
+    def get_by_category(self,category):
+        items = []
+        for item in self.inventory:
+            if category == item.category:
+                items.append(item)
+        return items
+
+#Wave 3 
+    def swap_items (self,friend, my_item, their_item):
+        if my_item not in self.inventory or their_item not in friend.inventory:
+            return False
+
+        else:
+            friend.inventory.remove(their_item)
+            self.inventory.append(their_item)
+            self.inventory.remove(my_item)
+            friend.inventory.append(my_item)
+            
+        return True 
+
+
+
+
+
