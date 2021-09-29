a482d292d618f06a371e6e4dc8554b2f41eb2811
Ainur Dzhaianbaeva <adjayanbaeva@gmail.com>
2021-09-29 11:26:26 -0700

    create condition_description() Item instance method, make Electronics, Clothing and Decor child classes of Item

--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -1,2 +1,9 @@
-class Electronics:
-    pass
+from swap_meet.item import Item
+
+class Electronics(Item):
+    def __init__(self, category = "Electronics"):
+        super().__init__(category)
+        self.category = category
+
+    def __str__(self):
+        return "A gadget full of buttons and secrets."
--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,10 +1,27 @@
 class Item:
-    def __init__(self, category = None):
+    def __init__(self, category = None, condition = None):
         if not category:
             category = ""
         self.category = category
+        if not condition:
+            condition = 0
+        self.condition = condition
 
     def __str__(self):
         return "Hello World!"
+
+    def condition_description(self):
+        if self.condition == 5:
+            print('mint')
+        elif self.condition == 4:
+            print('excellent, used only 1 time')
+        elif self.condition == 3:
+            print('good, used but works fine')
+        elif self.condition == 2:
+            print('heavily used, needs some repairment')
+        elif self.condition == 1:
+            print('not functioning, it can be your new project')
+        else:
+            print('no description available')
 
     
