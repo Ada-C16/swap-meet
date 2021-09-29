e464b5c9485925142966e7fb74592fa932101409
bailey <baileyholderman@gmail.com>
2021-09-29 12:16:08 -0700

    removes comments containing previous iteration adda attribute age

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,10 +1,8 @@
 from swap_meet.item import Item
 
 class Clothing(Item):
-    def __init__(self, condition = 0.0):
-        super().__init__(condition, category = "Clothing")
-        # self.category = category
-        # self.condition = condition
+    def __init__(self, condition = 0.0, age = None):
+        super().__init__(condition, age, category = "Clothing")
 
     def __str__(self):
         return "The finest clothing you could wear."
