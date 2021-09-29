af7f5eb45d330cc019bddb6de48e77c2ed91b3b9
Monica Cruz <mcatcruz@gmail.com>
2021-09-28 22:34:07 -0400

    Wave 2: fixed get_by_category. Passing all tests.

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,5 +1,3 @@
-from swap_meet.vendor import Vendor
-
 class Item:
     def __init__(self, category=""):
         self.category = category
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,8 +1,10 @@
 from swap_meet.item import Item
 
 class Vendor:
-    def __init__(self, inventory=[]):
+    def __init__(self, inventory=[], item=None):
         self.inventory = inventory
+        self.item = item
+
 
     def add(self, item):
         self.inventory.append(item)
@@ -15,5 +17,11 @@ class Vendor:
         else:
             return False
     
-    def get_by_category(self, category):
-        pass
+    def get_by_category(self, category=''):
+        categories_present_list = []
+        for i in range(len(self.inventory)):
+            if category == self.inventory[i].category: 
+                categories_present_list.append(self.inventory[i])
+            else:
+                continue
+        return categories_present_list
