cea2d4da621bb1baf5b786e4b55ee82be518e592
Khandice <s.khandice@gmail.com>
2021-09-29 11:05:03 -0700

    Updated Vendor class with get_by_category method, wave 2 tsts passing

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,8 +1,11 @@
+from .item import Item
+
 class Vendor:
-    def __init__(self, inventory=None):
+    def __init__(self, inventory=None, category=""):
         if not inventory:
             inventory = []
         self.inventory = inventory
+        self.category = category
     
     def add(self, item_add):
         self.inventory.append(item_add)
@@ -13,3 +16,10 @@ class Vendor:
             return False
         self.inventory.remove(item_remove)
         return item_remove
+
+    def get_by_category(self, category):
+        category_list = []
+        for item in self.inventory:
+            if item.category == category:
+                category_list.append(item)
+        return category_list
