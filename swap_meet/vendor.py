32a21d0fe13656b86e50e8a8f8e39c435191b5a5
Jesse <jesseannepope@gmail.com>
2021-09-29 16:08:21 -0700

    wave 6 progress

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -39,8 +39,30 @@ class Vendor:
         else:
             return False
 
-
+    def get_best_by_category(self, category):
+        items_by_category = self.get_by_category(category)
+        if len(items_by_category) == 0:
+            return None
+            
+        top_item_condition = 0
+        top_item = None
+        best_by_category = {}
         
+        for item in items_by_category:
+            best_by_category[item] = item.condition
+        for item, item_condition in best_by_category.items():
+            if item_condition > top_item_condition:
+                top_item_condition = item_condition
+                top_item = item
+        return top_item
+
+    def swap_best_by_category(self, other, my_priority, their_priority):
+        if len(self.get_by_category(their_priority)) == 0 or len(other.get_by_category(my_priority)) == 0:
+            return False
+        my_best_item = self.get_best_by_category(my_priority)
+        vendor_best_item = other.get_best_by_category(their_priority)
+        return self.swap_items(other, my_best_item, vendor_best_item)
+
         
 
 
