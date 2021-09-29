2a8aacc6c68dcebfd957b57892ab17a4d5771bb4
Jessica <jessica.w.roberts@gmail.com>
2021-09-28 21:06:03 -0700

    Refactor swap_items method in Vendor

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -17,12 +17,12 @@ class Vendor:
     def get_by_category(self, category):
         return [item for item  in self.inventory if item.category == category]
     
-    def swap_items(self, vendor, my_item, their_item):
-        if my_item in self.inventory and their_item in vendor.inventory:
+    def swap_items(self, Vendor, my_item, their_item):
+        if my_item in self.inventory and their_item in Vendor.inventory:
             self.remove(my_item)
             self.add(their_item)
-            vendor.remove(their_item)
-            vendor.add(my_item)
+            Vendor.remove(their_item)
+            Vendor.add(my_item)
             return True
         else: 
             return False
