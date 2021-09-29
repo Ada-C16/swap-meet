db13c9e74f42b50e46a6b4bb89e6a1d29b329653
Stephanie <stephanie.chenn1@gmail.com>
2021-09-29 17:29:31 -0400

    passed all tests in wave 6

--- main.py
+++ main.py
@@ -61,5 +61,40 @@ from swap_meet.item import Item
 # print(cloth.category)
 # print(str(cloth))
 
-decor = Decor(condition=3.5)
-print(decor.condition)
+# Testing Wave 6
+# item_a = Clothing(condition=2.0)
+# item_b = Decor(condition=2.0)
+# item_c = Clothing(condition=4.0)
+# item_d = Decor(condition=5.0)
+# item_e = Clothing(condition=3.0)
+# tai = Vendor(
+#     inventory=[item_a, item_b, item_c, item_d, item_e]
+# )
+
+# # [item_a, item_c, item_e]
+# best_item = tai.get_best_by_category("Clothing")
+# print(best_item.condition)
+
+# Testing Wave 6 Pt 2
+item_a = Decor(condition=2.0)
+item_b = Electronics(condition=4.0)
+item_c = Decor(condition=4.0)
+tai = Vendor(
+    inventory=[item_a, item_b, item_c]
+)
+
+item_d = Clothing(condition=2.0)
+item_e = Decor(condition=4.0)
+item_f = Clothing(condition=4.0)
+jesse = Vendor(
+    inventory=[item_d, item_e, item_f]
+)
+
+result = tai.swap_best_by_category(
+    other=jesse,
+    my_priority="Clothing",
+    their_priority="Decor"
+)
+for item in result:
+    print(item.category)
+    print(item.condition)
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,7 +1,7 @@
 class Vendor:
     def __init__(self, inventory = None):
         self.inventory = inventory if inventory is not None else []
-
+        
     def add(self,item):
         self.inventory.append(item)
         return item
@@ -39,3 +39,68 @@ class Vendor:
             return True
         else:
             return False
+
+    def get_best_by_category(self, category):
+        # get list of items with matching categories
+        matched_categories = []
+        for item in self.inventory:
+            if item.category == category:
+                matched_categories.append(item)
+
+        if not matched_categories:
+            return None
+        else:
+            best_condition = 0
+            best_item = matched_categories[0]
+            for matched_item in matched_categories:
+                if matched_item.condition > best_condition:
+                    best_condition = matched_item.condition
+                    best_item = matched_item
+                else:
+                    pass
+            return best_item
+
+    def swap_best_by_category(self, other, my_priority, their_priority):
+        items_to_swap_from_other = []
+        for item in other.inventory:
+            if item.category == my_priority:
+                items_to_swap_from_other.append(item)
+            else:
+                pass
+        
+        items_to_swap_for_other = []
+        for item in self.inventory:
+            if item.category == their_priority:
+                items_to_swap_for_other.append(item)
+            else:
+                pass
+        
+        if not items_to_swap_from_other or not items_to_swap_for_other:
+            return False
+        else:
+            best_item_in_other = items_to_swap_from_other[0]
+            other_best_condition = 0
+            for item in items_to_swap_from_other:
+                if item.condition > other_best_condition:
+                    other_best_condition = item.condition
+                    best_item_in_other = item
+                else:
+                    pass
+
+            best_item_in_mine = items_to_swap_for_other[0]
+            my_best_condition = 0
+            for item in items_to_swap_for_other:
+                if item.condition > my_best_condition:
+                    my_best_condition = item.condition
+                    best_item_in_mine = item
+                else:
+                    pass
+
+            other.inventory.remove(best_item_in_other)
+            self.inventory.append(best_item_in_other)
+            self.inventory.remove(best_item_in_mine)
+            other.inventory.append(best_item_in_mine)
+
+            return True
+
+
