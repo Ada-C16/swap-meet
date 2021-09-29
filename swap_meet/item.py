7046d36e1937e393d78a4a52973225d06ee3018e
Citlalli I Zarate <37793116+MiffyBruna@users.noreply.github.com>
2021-09-28 20:50:38 -0700

    All the tests from test_wave_05.py pass

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,6 +1,18 @@
 class Item():
-    def __init__(self, category =str("")):
+    def __init__(self, category =str(""), condition= 0):
         self.category = category
+        self.condition = condition
 
     def __str__(self):
         return "Hello World!"
+    
+    def condition_description(self):
+        description_rating= {0: "Fit for compost",
+        1: "Piece of Junk",
+        2: "Flawed and very used, but works",
+        3: "Visibly used might have some imperfections",
+        4: "Lightly used Item no imperfections",
+        5: "New"
+        }
+
+        return description_rating[self.condition]
