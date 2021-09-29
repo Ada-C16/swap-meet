0d67c0a000f952b8c22039f548cca74f5075eaee
starseed2021 <tiff.harper18@gmail.com>
2021-09-29 18:20:15 -0400

    Passed All Waves; Working on Xtra Credit; Created New Driver File

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,2 +1,51 @@
 class Item:
+    # Each Item will have an attribute named 'category'
+    # Category is an empty string by default
+    # After initialization, we can pass the keyword argument 'category'
     pass
+    def __init__(self, category = "", condition = 0, age = None):
+        self.category = category
+        self.condition = condition
+        self.age = age
+
+    # stringify (convert to string) and instance of Item 
+    # using str(), it returns "Hello World!"
+    def __str__(self):
+        return "Hello World!"
+
+    def condition_description(self):
+        # return string
+        if self.condition >= 5:
+            return "New Without Tags"
+        elif self.condition >= 4:
+            return "Mint Condition"
+        elif self.condition >= 3:
+            return "Gently Used"
+        elif self.condition >= 2:
+            return "Good Condition"
+        elif self.condition >= 1:
+            return "Some Visible Wear"
+        else:
+            return "Heavily Used"
+
+    def get_age(self):
+        if self.age >= 20:
+            return "Vintage"
+        elif self.age >= 10:
+            return "Retro"
+        elif self.age >= 5:
+            return "Modern"
+        elif self.age >= 1:
+            return "Contemporary"
+        else:
+            return "Brand New"
+
+    
+
+
+
+
+
+    
+    
+
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,77 @@
+
 class Vendor:
-    pass
+    def __init__(self, inventory=[]):
+      self.inventory = inventory
+
+    def add(self, item):
+        self.inventory.append(item)
+        return item
+
+    def remove(self, item):
+      if item in self.inventory:
+        self.inventory.remove(item)
+        return item
+      else:
+        return False
+
+    def get_by_category(self, category):
+        items_list = []
+        for item in self.inventory:
+          if item.category == category:
+            items_list.append(item)
+        return items_list
+
+    def swap_items(self, another_vendor, my_item, their_item):
+      if my_item not in self.inventory or their_item not in another_vendor.inventory:
+        return False
+      self.inventory.remove(my_item)
+      another_vendor.inventory.remove(their_item)
+      self.inventory.append(their_item)
+      another_vendor.inventory.append(my_item)
+      return True
+      
+    def swap_first_item(self, another_vendor):
+      if len(self.inventory) == 0 or len(another_vendor.inventory) == 0:
+        return False
+
+      my_first_item = self.inventory[0]
+      their_first_item = another_vendor.inventory[0]
+
+      return self.swap_items(another_vendor, my_first_item, their_first_item)
+
+    def get_best_by_category(self, category = ""):
+
+        items_by_category = self.get_by_category(category)
+
+        if len(items_by_category) == 0:
+          return None
+      
+        return max(items_by_category, key = lambda item : item.condition)
+  
+    def swap_best_by_category(self, other, my_priority, their_priority):
+      my_best_item = self.get_best_by_category(category = their_priority)
+
+      their_best_item = other.get_best_by_category(category = my_priority)
+
+      if len(self.inventory) == 0 or len(other.inventory) == 0:
+        return False
+
+      return self.swap_items(other, my_best_item, their_best_item)
+
+    def swap_by_newest(self, other, my_item, their_item):
+      pass
+      newest_item = self.get_age(their_item)
+      their_newest_item = other.get_age(my_item)
+
+
+
+      
+
+
+
+      
+
+
+
+
+
