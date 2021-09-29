cdc27ac38505a89cc708bd9849aee02c27a56851
Gabe Kelemen <gabe@gabekelemen.com>
2021-09-28 21:01:29 -0400

    fixes conditions function, wave 5 passes git status q

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,2 +1,12 @@
-class Clothing:
-    pass
+from swap_meet.item import Item
+
+class Clothing(Item):
+    def __init__(self, category = "Clothing", condition = 0.0):
+        # super().__init__(condition = 0)
+    
+        self.category = "Clothing"
+        self.condition = condition
+    
+    def __str__(self):
+        return "The finest clothing you could wear."
+
--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,3 +1,36 @@
 class Item:
-    def __init__(self, category = ""):
-        self.category = category
+    def __init__(self, category = "", condition = 0):
+        self.category = category 
+        self.condition = condition
+        
+        # Item.condition_description()
+        
+    def __str__(self):
+        return "Hello World!"
+    
+    def condition_description(self):
+        five_condition_desc = "go get it"
+        four_condition_desc = "only if you get rid of something else"
+        three_condition_desc = "only if you get rid of two other things"
+        two_condition_desc = "fuggedaboutit, yoove down sized"
+        one_condition_desc = "waste of time, move on"
+        zero_condition_desc = "ah hell no"
+        
+        if self.category:
+            if self.condition == 0.0:
+                return zero_condition_desc
+            
+            if self.condition <= 1.0:
+                return one_condition_desc
+                
+            elif self.condition <= 2.0:
+                return two_condition_desc
+                
+            if self.condition <= 3.0:
+                return three_condition_desc
+                
+            if self.condition <= 4.0:
+                return four_condition_desc
+                
+            if self.condition <= 5.0:
+                return five_condition_desc
