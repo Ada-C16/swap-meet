9a3ed848fa37e54ff74de215817bc5121bfc05ed
bailey <baileyholderman@gmail.com>
2021-09-29 12:17:30 -0700

    removes comments containing previous iteration adds attribute age

--- swap_meet/decor.py
+++ swap_meet/decor.py
@@ -1,13 +1,8 @@
 from swap_meet.item import Item
 
 class Decor(Item):
-    def __init__(self, condition = 0.0):
-        super().__init__(condition, category = "Decor")
-
-
-        # self.category = category
-        # self.condition = condition
-    
+    def __init__(self, condition = 0.0, age = None):
+        super().__init__(condition, age, category = "Decor")
 
     def __str__(self):
         return "Something to decorate your space."
