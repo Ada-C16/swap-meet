6a97fb9f7522db808d9977f215eacfbd82698846
Ngozi Amaefule <ngoziamaefule1@gmail.com>
2021-09-29 18:44:44 -0400

    All tests passed. DRYed up my code by implementing swap_items where code repeated

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -7,11 +7,11 @@ class Vendor:
         else:
             self.inventory = []
     
-    def add_item(self, item):
+    def add(self, item):
         self.inventory.append(item)
         return item
     
-    def remove_item(self, item):
+    def remove(self, item):
         if item in self.inventory:
             self.inventory.remove(item)
             return item
@@ -28,10 +28,40 @@ class Vendor:
     
     def swap_items(self, friend, my_item, their_item):
         if my_item in self.inventory and their_item in friend.inventory:
-            self.inventory.remove_item(my_item)
-            friend.inventory.remove_item(their_item)
-            friend.inventory.add_item(my_item)
-            self.inventory.add_item(their_item)
+            self.remove(my_item)
+            friend.remove(their_item)
+            friend.add(my_item)
+            self.add(their_item)
             return True
         else:
             return False
+
+    def swap_first_item(self, friend):
+        if self.inventory and friend.inventory:
+            self.swap_items(friend, self.inventory[0], friend.inventory[0])
+            return True
+
+    def get_best_by_category(self, category):
+        matching_category = self.get_by_category(category)
+        if matching_category == []:
+            return None
+        
+        best_condition = 0
+        best_item = None
+
+        for item in matching_category:
+            if item.condition > best_condition:
+                best_condition = item.condition
+                best_item = item
+        
+        return best_item
+    
+    def swap_best_by_category(self, other, my_priority, their_priority):
+        best_item_for_them = self.get_best_by_category(their_priority)
+        best_item_for_me = other.get_best_by_category(my_priority)
+        
+        if best_item_for_them is None or best_item_for_me is None:
+            return False
+        else:
+            self.swap_items(other, best_item_for_them, best_item_for_me)
+            return True
