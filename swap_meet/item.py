e06a00de272f20ad44a318d467d69e59a91d98c9
Mary <marytian.mt@gmail.com>
2021-09-28 23:40:46 -0600

    Wrote code for 5 waves. All tests passing.

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,2 +1,23 @@
 class Item:
-    pass
+    def __init__(self, category="", condition = 0):
+        self.category = category
+        self.condition = condition
+
+    def __str__(self):
+        return "Hello World!"
+
+    def condition_description(self):
+        if self.condition == 0:
+            return "Really Shitty"
+        elif self.condition == 1:
+            return "A Little Less Shitty"
+        elif self.condition == 2:
+            return "Almost Average"
+        elif self.condition == 3:
+            return "Above the Mendoza Line"
+        elif self.condition == 4:
+            return "Slightly Less Excellent"
+        elif self.condition == 5:
+            return "Excellent"
+
+        
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,49 @@
 class Vendor:
-    pass
+    
+    def __init__(self, inventory=[]):
+        self.inventory = inventory
+    
+    def add(self, item):
+        self.inventory.append(item)
+        return item
+    
+    def remove(self, item):
+        if item not in self.inventory:
+            return False
+        self.inventory.remove(item)
+        return item
+    
+    def get_by_category(self, category):
+        matching_items = []
+        for item in self.inventory:
+            if item.category == category:
+                matching_items.append(item)
+        return matching_items
+    
+    def swap_items(self, vendor, my_item, their_item):
+        if my_item not in self.inventory or their_item not in vendor.inventory:
+            return False
+        self.inventory.remove(my_item)
+        vendor.inventory.append(my_item)
+        vendor.inventory.remove(their_item)
+        self.inventory.append(their_item)
+        return True
+
+    def swap_first_item(self, vendor):
+        if len(self.inventory) == 0 or len(vendor.inventory) == 0:
+            return False
+
+        self_first_item = self.inventory[0]
+        friend_first_item = vendor.inventory[0]
+
+        self.inventory = self.inventory[1:]
+        vendor.inventory = vendor.inventory[1:]
+        self.inventory.append(friend_first_item)
+        vendor.inventory.append(self_first_item)
+        return True
+    
+  
+
+    
+        
+
