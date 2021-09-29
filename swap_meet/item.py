bb6974781d15dfb93cf14d58793cadd852418714
Rebecca Zuick <rebecca.zuick@gmail.com>
2021-09-29 09:40:21 -0500

    added condition_description function. passed all tests in wave 5

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -5,3 +5,20 @@ class Item:
 
     def __str__(self):
         return "Hello World!"
+
+    def condition_description(self):
+        if self.condition < 0.0 or self.condition > 5.0:
+            return "This is either really great or really gross. Proceed with caution."
+        
+        if self.condition == 1:
+            return "Yikes on a bike. You don't want this."
+        elif self.condition == 2:
+            return "This is barely acceptable. Like if you scraped mold off of old bread."
+        elif self.condition == 3:
+            return "Well, you could do worse. You could also do much better."
+        elif self.condition == 4:
+            return "Wow! This is actually good! Grab it before someone else does!"
+        elif self.condition == 5:
+            return "This is basically new. I hope this didn't fall off a truck somewhere..."
+        else:
+            return "This is in between. Is it worth it?"
