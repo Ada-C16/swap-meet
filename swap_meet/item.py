770879125d7c22dd6373efe084640b0bd1599fa5
Mariah Rosman <mrosman7@gmail.com>
2021-09-29 11:42:59 -0700

    adds and refactors condition_description() Item class method

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,2 +1,29 @@
 class Item:
-    pass
+    def __init__(self, category="", condition=0):
+        self.category = category
+        self.condition = condition
+
+    def __str__(self):
+        return "Hello World!"
+
+    def condition_description(self):
+        try:
+            if self.condition == 0.0:
+                return "This has seen better days"
+            elif self.condition == 1.0:
+                return "Eh...maybe go with something else"
+            elif self.condition == 2.0:
+                return "Not terrible but not great"
+            elif self.condition == 3.0:
+                return "Average"
+            elif self.condition == 4.0:
+                return "Pretty good!"
+            elif self.condition == 5.0:
+                return "As good as it gets!"
+            else:
+                raise ValueError
+        
+        except ValueError:
+            print("The condition must be a number between 0 and 5")
+
+
