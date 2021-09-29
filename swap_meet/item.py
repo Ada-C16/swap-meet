576f0485c0ee5fb10fbd68d52359d7817f6976e8
Alma <abecerrilsalas@gmail.com>
2021-09-29 11:22:10 -0700

    wave 1,2,3

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,2 +1,10 @@
-class Item:
-    pass
+
+
+
+class Item:#might have to chage and add the conditional
+    def __init__(self, category=""):
+
+        self.category=category
+    
+    def __str__(self):
+        return "Hello World!"
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,43 @@
+from swap_meet.item import Item
+
 class Vendor:
-    pass
+    def __init__(self, inventory = None):
+        # self.item = Item()
+        if not inventory:
+            inventory=[]
+        self.inventory=inventory 
+        
+
+
+    def add(self, item):
+        self.inventory.append(item)
+        return item
+    
+    def remove(self, item):
+        
+        if item not in self.inventory:
+            return False
+            
+        else:
+            self.inventory.remove(item)
+            return item
+
+    def get_by_category(self, category):
+        
+        category_list=[] 
+        for item in self.inventory:
+            if item.category == category:
+                category_list.append(item)
+        return category_list
+    
+    def swap_items(self, vendor, my_item, their_item):
+        if my_item in self.inventory and their_item in vendor.inventory:
+            self.inventory.append(their_item)
+            vendor.inventory.append(my_item)
+            self.inventory.remove(my_item)
+            vendor.inventory.remove(their_item)
+            return True
+
+        return False
+
+
