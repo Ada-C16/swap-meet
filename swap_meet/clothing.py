c98c16639faca04e1fce3505e6c7a6c2f661474c
sarahstandish <z.sisyphusrocks@gmail.com>
2021-09-29 15:36:46 -0700

    fix typo

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -3,7 +3,7 @@ from swap_meet.item import Item
 class Clothing(Item):
 
     def __init__(self, condition=0, age=0):
-        super().__init__(condition, age, category="Clothing")
+        super().__init__(condition, age, category="Clothing", )
     
     def __str__(self):
         return "The finest clothing you could wear."
