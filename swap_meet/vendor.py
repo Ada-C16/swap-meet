9436ec50e1692e52d32efe1276af105b90bb58af
Citlalli I Zarate <37793116+MiffyBruna@users.noreply.github.com>
2021-09-28 17:10:10 -0700

    All the tests from test_wave_02 pass, created instance method get_by_category in vendor.py and initialized and initialized the class named Item which passes category

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -23,3 +23,12 @@ class Vendor:
             if item.category ==  category:
                 items_by_category.append(item)
         return items_by_category
+    
+
+    def swap_items(self, vendor, my_item, their_item):
+        if my_item not in self.inventory or their_item not in vendor.inventory:
+            return False
+        else:
+            self.add(their_item) and self.remove(my_item)
+            vendor.add(my_item) and vendor.remove(their_item)
+            return True
