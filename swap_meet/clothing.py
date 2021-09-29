f59c12858a1bca8b9596ecd31c768ec1716a3bbc
Areeba Rashid <areeba.r.qureshi@gmail.com>
2021-09-29 14:15:54 -0700

    Updated with proper constructor super call in child classes

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -2,8 +2,8 @@ from .item import Item
 class Clothing(Item):
 
     def __init__(self, condition = 0):
-        super().__init__(self, condition = condition)
-        self.category = "Clothing"
+        super().__init__(category = "Clothing", condition = condition)
+        #self.category = "Clothing"
 
     def __str__(self):
         return "The finest clothing you could wear."
--- swap_meet/decor.py
+++ swap_meet/decor.py
@@ -1,8 +1,8 @@
 from .item import Item
 class Decor(Item):
     def __init__(self, condition = 0):
-        super().__init__(self, condition = condition)
-        self.category = "Decor"
+        super().__init__(category = "Decor", condition = condition)
+        #self.category = "Decor"
 
     def __str__(self):
         return "Something to decorate your space."
