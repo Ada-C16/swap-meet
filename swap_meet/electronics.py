122bf91197cec625088dec42a6d2ce4eeebc6692
Rebecca Zuick <rebecca.zuick@gmail.com>
2021-09-29 09:39:56 -0500

    passed all tests in wave 5. added condition attribute and used inheritance to access condition_description function

--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -1,6 +1,13 @@
-class Electronics:
-    def __init__(self) -> None:
-        self.category = "Electronics"
+from .item import Item
 
-    def __str__(self) -> str:
+class Electronics(Item):
+    def __init__(self, condition = 0):
+        super().__init__()
+        self.category = "Electronics"
+        self.condition = condition if condition else condition is 0
+
+    def __str__(self):
         return "A gadget full of buttons and secrets."
+
+    def condition_description(self):
+        return super().condition_description()
