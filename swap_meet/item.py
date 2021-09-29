75b342710b894ab6fb24d9b0a54991d5916cbda6
Makhabat <maksatbekova.m@gmail.com>
2021-09-29 09:24:23 -0700

    Finish project

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,2 +1,19 @@
-class Item:
-    pass
+
+class Item():
+    def __init__(self, category = "", condition=0, age=0):
+        self.category = category
+        self.condition = condition 
+        self.age = age
+
+    def __str__(self):
+        return ("Hello World!")
+
+    def condition_description(self):
+        if self.condition <= 2:
+            return "used - normal"
+        elif self.condition <= 3:
+            return "good"
+        elif self.condition <= 4:
+            return "heavily used"
+        else:
+            return "mint"
