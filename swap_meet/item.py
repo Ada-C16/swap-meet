76c8d50173b9e2fa67fc7c1d72afdec9d23503ce
sarahstandish <z.sisyphusrocks@gmail.com>
2021-09-29 14:59:32 -0700

    add age parameter to items

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,6 +1,6 @@
 class Item:
     
-    def __init__(self, condition=0.0, category=""):
+    def __init__(self, condition=0, category="", age=0):
 
         self.category = category.title()
         self.condition = condition
