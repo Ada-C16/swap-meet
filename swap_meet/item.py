86a352c25a0273716c38f9a404265e4342c643ed
Khandice <s.khandice@gmail.com>
2021-09-29 11:32:58 -0700

    updated Item class object to return 'Hello World'

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,6 +1,10 @@
 class Item:
-    def __init__(self, category="", item_name=""):
+    def __init__(self, category="", item_name="", id="Hello World!"):
+        self.id = id
         self.category = category
         self.item_name = item_name
+    
+    def __str__(self):
+        return str(self.id)
         
 
