3a585dce0be82bd31d6b5c31e35909cba69f485c
jacy <jacyyang04@gmail.com>
2021-09-28 17:53:48 -0700

    add methods and defined item class for wave 2

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from swap_meet.vendor import Vendor
 from swap_meet.item import Item
 
-vendor = Vendor()
-item = Item()
+item_a = Item(category="clothing")
+item_b = Item(category="clothing")
+item_c = Item(category="clothing")
+vendor = Vendor(
+    inventory=[item_a, item_b, item_c]
+)
 
+items = vendor.get_by_category("electronics")
 
-print(vendor.inventory)
-#print(item)
+print(items)
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,16 +1,34 @@
-class Vendor:
-    def __init__(self, inventory=None):
-        if not inventory:
-            inventory = []
-        self.inventory = inventory
+from swap_meet.item import Item
 
+class Vendor():
+    def __init__(self, inventory=None):
+        if not inventory: #if not None
+            inventory = []
+        #instantiate attributes
+        self.inventory = inventory
+        self.item = Item()
+
+    #instantiate methods
+
+    #adds item to inventory list
     def add(self, item):
         self.inventory.append(item)
         return item
 
+    #removes item from inventory list
     def remove(self, item):
         if item not in self.inventory:
             return False
         
         self.inventory.remove(item)
         return item
+
+    #return inventory list with given category
+    def get_by_category(self, category):
+        #return inventory for given item.category
+        inventory_category = []
+
+        for self.item in self.inventory:
+            if self.item.category == category:
+                inventory_category.append(self.item)
+        return inventory_category
