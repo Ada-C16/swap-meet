32d5bbf2a6fad58a51a654e2d2b12837c755471a
tt-ht <ttht@protonmail.com>
2021-09-29 10:57:50 -0700

    refactored to use super()

--- main.py
+++ main.py
@@ -3,7 +3,3 @@ from swap_meet.decor import Decor
 from swap_meet.electronics import Electronics
 from swap_meet.item import Item
 from swap_meet.vendor import Vendor
-
-
-clothing = Clothing()
-decor = Decor()
--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,7 +1,7 @@
 class Item:
-    def __init__(self, category = "", condition = 0):
-        self.category = category
+    def __init__(self, condition = 0, category = ""):
         self.condition = condition
+        self.category = category
 
     def __str__(self):
         return "Hello World!"
