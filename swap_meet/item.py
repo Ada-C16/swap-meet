c1e127b60691ccd96a7d7ab215f89a5628836b72
Jessica <jessica.w.roberts@gmail.com>
2021-09-30 11:20:47 -0700

    Add condition_description method to class Item.

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -6,5 +6,17 @@ class Item:
     def __str__(self):
         return "Hello World!"
 
-
-    
+    def condition_description(self):
+        if 0 <= self.condition < 1:
+            return "Straight to the trash!"
+        elif 1 <= self.condition < 2:
+            return "Has a history"
+        elif 2 <= self.condition < 3:
+            return "So So"
+        elif 3 <= self.condition < 4:
+            return "Pretty good"
+        elif 4 <= self.condition <=5:
+            return "Woah, fancy!"
+        else:
+            return None
+            
