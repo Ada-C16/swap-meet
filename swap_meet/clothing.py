794fdf470b07196f41694d6101a6ad9396791878
Ro Peeples <rogpeeples@gmail.com>
2021-09-30 11:14:42 -0400

    Ro's Swap Meet With Super

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,13 +1,18 @@
 from swap_meet.item import Item
 
 class Clothing(Item):
-    def __init__(self, category = "Clothing", condition = 0):
-        self.category = category
-        self.condition = condition
+    def __init__(self, condition = 0):
+        # self.category = category
+        # self.condition = condition
 
-        #super().__init__(category = "Clothing", conditon = condition)
+        super().__init__(category = "Clothing", condition = condition) #do not use self #will get a weird error #assumes self is another argument 
 
         #super().condition_description()
+
+        #super starts the lookup function the next level higher (in this case, Item)
+        #we don't need line 10 because clothing isn't overriding the condition_description method
+        #only need to use super if class has a method with the same name as the parent, and we need to call the 
+        #method in the parent class 
 
     def __str__(self):
         return "The finest clothing you could wear."
