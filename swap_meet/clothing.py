48681f30ef0035d303892383a1ead4f9c6d306ab
Khandice <s.khandice@gmail.com>
2021-09-29 20:34:35 -0700

    Updated Clothing and Decor classes to have cetgory attributes, optional condition attribute and added strigify return string for each class object

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,3 +1,8 @@
 class Clothing:
-    def __init__(self, category="Clothing"):
+    def __init__(self, category="Clothing", condition=0, id="The finest clothing you could wear."):
         self.category = category
+        self.id = id
+        self. condition = condition
+    
+    def __str__(self):
+        return str(self.id)
--- swap_meet/decor.py
+++ swap_meet/decor.py
@@ -1,2 +1,8 @@
 class Decor:
-    pass
+    def __init__(self, category="Decor", condition=0, id="Something to decorate your space."):
+        self.category = category
+        self.id = id
+        self. condition = condition
+
+    def __str__(self):
+        return str(self.id)
