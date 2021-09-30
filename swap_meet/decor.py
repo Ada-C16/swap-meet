b6da64cc40bf5ce379ed469249ae9c662582daf9
Rebecca Zuick <rebecca.zuick@gmail.com>
2021-09-30 15:52:19 -0500

    changed is to == per warning message in Learn

--- swap_meet/decor.py
+++ swap_meet/decor.py
@@ -4,8 +4,8 @@ class Decor(Item):
     def __init__(self, condition = 0, age = 0):
         super().__init__()
         self.category = "Decor"
-        self.condition = condition if condition else condition is 0
-        self.age = age if age else age is 0
+        self.condition = condition if condition else condition == 0
+        self.age = age if age else age == 0
 
     def __str__(self):
         return "Something to decorate your space."
