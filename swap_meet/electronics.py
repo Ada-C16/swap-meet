e6b2bf49af3eb902a7a54e05771463205a2ca159
roza ajema <mentoroza@yahoo.com>
2021-09-30 16:10:19 -0700

    comments added

--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -1,8 +1,8 @@
 from .item import Item
 
 class Electronics(Item):
-    def __init__(self,category = "", condition = 0):
-        super().__init__(category = "Electronics",condition = condition)
+    def __init__(self,category = "", condition = 0): #setting category = "Electronics here leaves a room for change when objects are created"
+        super().__init__(category = "Electronics",condition = condition) #whatever set here is the final value to be set to this objects attribute
        
     def __str__(self):
         return "A gadget full of buttons and secrets."
