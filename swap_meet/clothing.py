1ba0f303da4d1d7f38dcb16ac08d9e0d5dd3ffa9
Mariah Rosman <mrosman7@gmail.com>
2021-09-30 15:38:56 -0700

    adds comments to class Clothing

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,11 +1,15 @@
 from .item import Item
 
 class Clothing(Item):
+    """
+    Class Clothing, subclass of class Item
+    """ 
+    # class constructor method & super method
     def __init__(self, condition=0):
         super().__init__(condition=condition)
         self.category = "Clothing"
 
-
+    # stringify instances of class Clothing
     def __str__(self):
         return "The finest clothing you could wear."
 
