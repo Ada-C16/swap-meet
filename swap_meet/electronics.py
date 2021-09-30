1138dace5e4e52d1c22dbe440f9efec54e65bde6
Mariah Rosman <mrosman7@gmail.com>
2021-09-30 15:38:05 -0700

    adds comments to class Electronics

--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -1,9 +1,14 @@
 from .item import Item
 
 class Electronics(Item):
+    """
+    Class Electronics, subclass of class Item
+    """   
+    # class constructor method & super method 
     def __init__(self, condition=0):
         super().__init__(condition = condition)
         self.category = "Electronics"
 
+    # stringify instances of class Electronics
     def __str__(self):
         return "A gadget full of buttons and secrets."
