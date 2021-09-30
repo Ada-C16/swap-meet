3434cdbdb3db30e5cadc027126e84f5fd3cf24a8
Emily Colon <emiquigs@gmail.com>
2021-09-30 18:17:43 +1300

    All wave 2,3,4,5 tests pass

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,2 +1,15 @@
 class Item:
-    pass
+    def __init__(self, category=None, condition = 0):
+        self.category = category if category is not None else ""
+        self.condition = condition
+    
+    def __str__(self):
+        return "Hello World!"
+
+    def condition_description(self):
+        if self.condition < 2:
+            return "This is heavily used."
+        if self.condition is 3-4:
+            return "This is in mint condition."
+        if self.condition >4:
+            return "This is in excellent condition."
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,6 +1,8 @@
+from swap_meet.item import Item
+
 class Vendor:
-    def __init__(self, inventory=[]):
-        self.inventory = inventory
+    def __init__(self, inventory=None):
+        self.inventory = inventory if inventory is not None else []
 
     def add(self, item):
         self.inventory.append(item)
@@ -12,3 +14,31 @@ class Vendor:
         else:
             self.inventory.remove(item)
             return item
+
+    def get_by_category(self, one_category):
+        items= [item for item in self.inventory if item.category == one_category]
+        return items
+
+    def swap_items(self, friend, my_item, their_item):
+        if my_item not in self.inventory or their_item not in friend.inventory:
+            return False
+        else:
+            self.inventory.append(their_item)
+            friend.inventory.remove(their_item)
+            friend.inventory.append(my_item)
+            self.inventory.remove(my_item)
+            return True
+    
+    def swap_first_item(self, friend):
+        if not self.inventory or not friend.inventory:
+            return False
+        
+        else:
+            self.inventory.append(friend.inventory[0])
+            friend.inventory.append(self.inventory[0])
+            self.inventory.pop(0)
+            friend.inventory.pop(0)
+            return True
+
+
+
