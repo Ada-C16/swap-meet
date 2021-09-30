1495229706b0413353e17a460a75a11877c35ae9
jacy <jacyyang04@gmail.com>
2021-09-30 08:57:13 -0700

    add get_best_by_category function

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -35,8 +35,9 @@ class Vendor():
         return inventory_category
 
 
+    #swap given items if items are not in designated inventories
     def swap_items(self, other_vendor, my_item=None, their_item=None):
-        #if any items are None, return False
+        #if any items are in designated inventory, return False
         if my_item not in self.inventory or their_item not in other_vendor.inventory:
             return False
     
@@ -45,12 +46,35 @@ class Vendor():
         other_vendor.remove(their_item)
         self.add(their_item)
         return True
-            
 
 
+    #returns True/False if inventories are not empty
     def swap_first_item(self, other_vendor):
-        if self.inventory==[] or other_vendor.inventory==[]:
+        if self.inventory or other_vendor.inventory:
             return False
 
         self.swap_items(other_vendor, self.inventory[0], other_vendor.inventory[0])
         return True
+
+    #returns item with the best condition in given category
+    def get_best_by_category(self, category):
+        category_list = []
+
+        #if inventory is not False, return None
+        if self.inventory==[]:
+            return None
+
+        #iterate through self.inventory
+            #checks for self.condition and matching category
+        for item in self.inventory:
+            if item.category == category:
+                category_list.append(item)
+        
+        #returns max condition
+        if category_list==[]:
+            return None
+        
+        return max(category_list, key=lambda item : item.condition)
+
+
+    
