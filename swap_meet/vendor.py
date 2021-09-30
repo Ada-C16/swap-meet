7c4aaca4611f62c47717afce3ef2f841c7185cc8
bailey <baileyholderman@gmail.com>
2021-09-30 16:46:36 -0700

    changes other to vendor_2 for consistency

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -28,8 +28,8 @@ class Vendor:
   def get_best_by_category(self, category):
     return max(self.get_by_category(category), key = lambda a: a.condition, default = None)
 
-  def swap_best_by_category(self, other, my_priority, their_priority):
-    return self.swap_items(other, self.get_best_by_category(their_priority), other.get_best_by_category(my_priority))
+  def swap_best_by_category(self, vendor_2, my_priority, their_priority):
+    return self.swap_items(vendor_2, self.get_best_by_category(their_priority), vendor_2.get_best_by_category(my_priority))
   
   def get_newest_in_inventory(self):
     return min(self.inventory, key = lambda item : item.age, default = None)
