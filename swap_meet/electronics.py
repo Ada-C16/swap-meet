3f8098bb029f4b53d86f55eebd658e0c9190616d
Rebecca Zuick <rebecca.zuick@gmail.com>
2021-09-30 15:52:41 -0500

    changed is to == per warning message in Learn

--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -4,8 +4,8 @@ class Electronics(Item):
     def __init__(self, condition = 0, age = 0):
         super().__init__()
         self.category = "Electronics"
-        self.condition = condition if condition else condition is 0
-        self.age = age if age else age is 0
+        self.condition = condition if condition else condition == 0
+        self.age = age if age else age == 0
 
     def __str__(self):
         return "A gadget full of buttons and secrets."
