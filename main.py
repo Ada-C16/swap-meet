41a3c28bd641795229e140b071d2c8bc32a4b4c5
Mariah Rosman <mrosman7@gmail.com>
2021-09-29 20:35:58 -0700

    updates driver file with instances of classes

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
 from swap_meet.vendor import Vendor 
 from swap_meet.item import Item
+from swap_meet.clothing import Clothing
 
-item1 = Item("clothing")
+item1 = Item("Clothing", 5)
+item2 = Item("Electronics", 1)
+item3 = Item("Clothing", 2)
 
-print(item1.category)
+item4 = Item("Decor", 5)
+item5 = Item("Clothing", 4)
+item6 = Item("Electronics", 2)
 
-vendor1 = Vendor()
+# print(item1.category)
+# print(item2.category)
+# print(item3.category)
 
-print(vendor1.inventory)
+vendor1 = Vendor([item1, item2, item3])
+other = Vendor([item4, item5, item6])
+
+# print(vendor1.inventory)
+
+
+cloth = Clothing(condition=3.5)
+print(cloth.condition)
+
+print(type(item1.condition))
+print(f"This is my test: ") 
+print(vendor1.swap_best_by_category(other, 'Decor', 'Clothing'))
