358b9d25d556417f33203002b443c2dfb319f020
Khandice <s.khandice@gmail.com>
2021-09-29 20:37:21 -0700

    Updated Electronics Class with category attribute, optional condition attribute and  added stringify return string to class object

--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -1,2 +1,8 @@
 class Electronics:
-    pass
+    def __init__(self, category="Electronics", condition=0, id="A gadget full of buttons and secrets."):
+        self.category = category
+        self.id = id
+        self. condition = condition
+
+    def __str__(self):
+        return str(self.id)
