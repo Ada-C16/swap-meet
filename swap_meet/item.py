644a76e43088ba0fdfe1982e6c469e01fcbba644
dynamicdev3000 <michellemoralesjs@gmail.com>
2021-09-29 21:38:29 -0400

    all waves passed, added age, working on merging swap items and swap first item

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,22 +1,35 @@
 class Item:
-    def __init__(self, category= "", condition = 0):
+    def __init__(self, category= "", condition = 0, age = None):
         self.category = category
         self.condition = condition
+        self.age = age
 
     def __str__(self):
         return "Hello World!"
 
     def condition_description(self):
         if self.condition >= 5:
-            return "Brand Spanking New!"
+            return "Brand spanking new!"
         elif self.condition >= 4:
-            return "Brand New"
+            return "Brand new"
         elif self.condition >=3:
-            return "Looks good!"
+            return "Looks nice!"
         elif self.condition >= 2:
-            return "Alright"
+            return "Good"
         elif self.condition >= 1:
             return "Acceptable"
         else:
-            return "buy at your own risk"
+            return "Clearance item - no returns accepted.  Buy at your own risk!"
+    
+    def get_age(self):
+        if self.age >= 20:
+            return "Vintage"
+        elif self.age >= 10:
+            return "Retro"
+        elif self.age >= 5:
+            return "Contemporary"
+        elif self.age >=1:
+            return "Trending now"
+        else:
+            return "Brand new!"
 
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -27,11 +27,12 @@ class Vendor:
     def swap_items(self, other_vendor, my_item, their_item):
         if my_item not in self.inventory or their_item not in other_vendor.inventory:
             return False
+            
         else:
-            self.inventory.remove(my_item)
             other_vendor.inventory.append(my_item)
-            other_vendor.remove(their_item)
+            self.inventory.remove(my_item)
             self.inventory.append(their_item)
+            other_vendor.remove(their_item)
             return True
     
     def swap_first_item(self, other_vendor):
@@ -44,5 +45,44 @@ class Vendor:
         else:
             return False 
 
+        # if self.inventory == [] or other_vendor.inventory == []:
+        #     return False
+        # elif len(self.inventory) != 0 and len(other_vendor.inventory) != 0: 
+        #     other_vendor.inventory.append(self.inventory[0])
+        #     self.inventory.remove(self.inventory[0])
+        #     self.inventory.append(other_vendor.inventory[0])
+        #     other_vendor.inventory.remove(other_vendor.inventory[0])
+        #     return True
+
     def get_best_by_category(self, category =""):
-        pass
+        items_by_category = self.get_by_category(category)
+        if len(items_by_category) == 0:
+            return None
+        return max(items_by_category, key=lambda item: item.condition)
+    
+    def swap_best_by_category(self, other, my_priority, their_priority):
+        best_item = self.get_best_by_category(category=their_priority)
+        their_best_item = other.get_best_by_category(category=my_priority)
+        if len(self.inventory) == 0 or len(other.inventory) == 0:
+            return False
+        return self.swap_items(other, best_item, their_best_item)
+
+    def get_by_age(self, age):
+        items = []
+        for item in self.inventory:
+            if item.age == age:
+                items.append(item)
+        return items
+    
+    def get_newest_item(self, age = ""):
+        items_by_age = self.get_by_age(age)
+        if len(items_by_age) == 0:
+            return None
+        return min(items_by_age, key=lambda item: item.age)
+    
+    def swap_by_newest(self, other, my_item, their_item):
+        my_newest = self.get_newest_item(age=my_item)
+        their_newest = self.get_newest_item(age=their_item)
+        if len(self.inventory) == 0 or len(other.inventory) == 0:
+            return False
+        return self.swap_items(other, my_newest, their_newest)
