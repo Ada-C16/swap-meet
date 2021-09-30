355d8d24d7a28bf819f2f17b9e58f49103ffbe7b
Mac <mackenziespeyton@gmail.com>
2021-09-30 09:58:00 -0700

    Refactored with ternary operator and list comprehension

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -2,9 +2,8 @@ from .item import Item
 
 class Vendor:
     def __init__(self, inventory = None):
-        if not inventory: 
-            inventory = []
-        self.inventory = inventory # List of item objects
+        inventory = [] if not inventory else inventory
+        self.inventory = inventory
 
     def add(self, item):
         """
@@ -20,36 +19,29 @@ class Vendor:
         """
         if item not in self.inventory:
             return False 
+
         self.inventory.remove(item)
+        
         return item
     
     def get_by_category(self, category):
         """
         Returning items within a given category
         """
-        category_items_list = []
-
-        for item in self.inventory: 
-            if item.category == category:
-                category_items_list.append(item)
-        
-        return category_items_list
+        return [item for item in self.inventory if item.category == category]
 
     def swap_items(self, other_vendor, my_item, their_item):
         """
         Swapping items between two vendors. 
         Returns false if items not in one or both inventories. 
         """
-        # Returning false if items not in one or both inventories. 
         if my_item not in self.inventory or \
-        their_item not in other_vendor.inventory: 
+        their_item not in other_vendor.inventory:                   # How to use ternary operator here?
             return False
 
-        # Swapping my_item
         self.remove(my_item)
         other_vendor.add(my_item)
 
-        # Swapping their_item
         self.add(their_item)
         other_vendor.remove(their_item)
 
@@ -60,16 +52,13 @@ class Vendor:
         Swapping first item in vendors' inventories. 
         Returns false if either inventory is empty. 
         """
-        # Returning false if either inventory is empty. 
         if len(self.inventory) == 0 or \
         len(other_vendor.inventory) == 0:
             return False
 
-        # Initializing variables to represent first item in vendors' inventories.
         my_first_item = self.inventory[0]
         their_first_item = other_vendor.inventory[0]
 
-        # Calling swap_items with first item in inventory variables set. 
         result = self.swap_items(other_vendor, my_first_item, their_first_item)
         
         return result
@@ -78,37 +67,31 @@ class Vendor:
         """
         Returns item with highest condition score within a category.
         """
-        # Initializing variables
         items_in_category = self.get_by_category(category)
         highest_condition = 0.0
         best_item = ""
 
-        # Returning None if there are no items in the category
         if not items_in_category: 
             return None 
 
-        # Identifying item with highest condition rating.
         for item in items_in_category:
             if item.condition > highest_condition:
                 highest_condition = item.condition
-                best_item =item
+                best_item = item
 
         return best_item
 
-    def swap_best_by_category(self, other, my_priority, their_priority):
+    def swap_best_by_category(self, other_vendor, my_priority, their_priority):
         """
         Returns True if both vendors trade best conditioned item from preferred cateogry. 
         Returns False if either vendor doesn't have an item in the other's preferred cateogry. 
         """
-        # Identifying best conditioned item in inventory based on other vendor's preferred category.
-        taken_item = other.get_best_by_category(my_priority)
+        taken_item = other_vendor.get_best_by_category(my_priority)
         given_item = self.get_best_by_category(their_priority)
 
-        # Returning False if either vendor doesn't have an item in the other's preferred category. 
         if not taken_item or not given_item:
             return False 
 
-        # Swapping best items in preferred category. 
-        self.swap_items(other, given_item, taken_item)
+        self.swap_items(other_vendor, given_item, taken_item)
 
         return True
