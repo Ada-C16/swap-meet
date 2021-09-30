c4c08880ef6f992f648148600a79102040953bec
Jesse <jesseannepope@gmail.com>
2021-09-30 09:32:16 -0700

    Completed waves, waiting for refactoring

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -3,7 +3,7 @@ from .item import Item
 class Clothing(Item):
     def __init__(self, condition=0):
         super().__init__(category="Clothing", condition=condition)
-        self.condition = condition
+        # self.condition = condition
     
     def __str__(self):
         return f"The finest clothing you could wear."
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -41,7 +41,7 @@ class Vendor:
 
     def get_best_by_category(self, category):
         items_by_category = self.get_by_category(category)
-        if len(items_by_category) == 0:
+        if items_by_category == None:
             return None
             
         top_item_condition = 0
@@ -59,9 +59,15 @@ class Vendor:
     def swap_best_by_category(self, other, my_priority, their_priority):
         if len(self.get_by_category(their_priority)) == 0 or len(other.get_by_category(my_priority)) == 0:
             return False
-        my_best_item = self.get_best_by_category(my_priority)
-        vendor_best_item = other.get_best_by_category(their_priority)
-        return self.swap_items(other, my_best_item, vendor_best_item)
+
+        my_best_item = self.get_best_by_category(their_priority)
+        vendor_best_item = other.get_best_by_category(my_priority)
+
+        # if not my_best_item or not vendor_best_item:
+        #     return False
+
+        self.swap_items(other, my_best_item, vendor_best_item)
+        return True
 
         
 
