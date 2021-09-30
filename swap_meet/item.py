79e8f66e6e3cee05f6695f5153ea184c7c4d7bff
Rebecca Zuick <rebecca.zuick@gmail.com>
2021-09-30 15:40:48 -0500

    added age attribute to item and age condition function. applied this across other item classes with super()

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,7 +1,8 @@
 class Item:
-    def __init__(self, category = "", condition = 0):
+    def __init__(self, category = "", condition = 0, age = 0):
         self.category = category
         self.condition = condition
+        self.age = age
 
     def __str__(self):
         return "Hello World!"
@@ -22,3 +23,19 @@ class Item:
             return "This is basically new. I hope this didn't fall off a truck somewhere..."
         else:
             return "This is in between. Is it worth it?"
+    
+    def age_description(self):
+        if self.age < 0:
+            return "This...should not be for sale"
+        elif self.age >= 50:
+            return "This is an antique!"
+        elif self.age >= 25:
+            return "This has led a decent life"
+        elif self.age >= 15:
+            return "This is old enough to drive"
+        elif self.age >= 10:
+            return "Wow, what a decade!"
+        elif self.age >=5:
+            return "You could get some good mileage out of this."
+        else:
+            return "This is pretty new! Nice!"
