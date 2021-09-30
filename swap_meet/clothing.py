0bbd39a8e431b83da1942b8ec7a359914d1fbe29
Ro Peeples <rogpeeples@gmail.com>
2021-09-30 10:23:47 -0400

    Ro's Swap Meet

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,2 +1,13 @@
-class Clothing:
-    pass
+from swap_meet.item import Item
+
+class Clothing(Item):
+    def __init__(self, category = "Clothing", condition = 0):
+        self.category = category
+        self.condition = condition
+
+        #super().__init__(category = "Clothing", conditon = condition)
+
+        #super().condition_description()
+
+    def __str__(self):
+        return "The finest clothing you could wear."
--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -1,2 +1,14 @@
-class Electronics:
-    pass
+from swap_meet.item import Item
+
+class Electronics(Item):
+    def __init__(self, category = "Electronics", condition = 0):
+        self.category = category
+        self.condition = condition
+
+        #super().__init__(category = "Clothing", conditon = condition)
+
+        #super().condition_description()
+
+    def __str__(self):
+        return "A gadget full of buttons and secrets."
+
