4a2d18dc95ef0b0c603bcdbd9d172f82965e6b87
Sophia Le <sophiale@sophias-mbp.lan>
2021-09-30 15:59:47 -0700

    extend Item condition_description() method

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -8,4 +8,27 @@ class Item:
         return description
 
     def condition_description(self):
-        return f'The item condition is {self.condition}.'
+        CONDITION_STATEMENTS = {
+            0: 'Probably it has no rating yet',
+            1: 'We blame bad service',
+            2: 'Barely acceptable',
+            3: 'Average',
+            4: 'Great product, and great service',
+            5: 'Paid reviews',
+            6: 'Rating value out of bound'
+        }
+        condition_statement = CONDITION_STATEMENTS[6]
+        if self.condition <= 0.5:
+            condition_statement = CONDITION_STATEMENTS[0]
+        elif self.condition <= 1.5:
+            condition_statement = CONDITION_STATEMENTS[1]
+        elif self.condition <= 2.5:
+            condition_statement = CONDITION_STATEMENTS[2]
+        elif self.condition <= 3.5:
+            condition_statement = CONDITION_STATEMENTS[3]
+        elif self.condition <= 4.5:
+            condition_statement = CONDITION_STATEMENTS[4]
+        elif self.condition <= 5.0:
+            condition_statement = CONDITION_STATEMENTS[5]
+
+        return condition_statement
