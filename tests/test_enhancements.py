a280b799d8177d9fda7a6cc88f3b8731cfed050d
Shay <shay.phillips11@gmail.com>
2021-09-30 16:49:27 -0400

    adds test for default age

--- tests/test_enhancements.py
+++ tests/test_enhancements.py
@@ -4,7 +4,12 @@ from swap_meet.vendor import Vendor
 from swap_meet.clothing import Clothing
 from swap_meet.decor import Decor
 from swap_meet.electronics import Electronics
+from swap_meet.item import Item
 
+
+def test_items_have_default_age_negative_one():
+    item = Item()
+    assert item.age == -1
 
 def test_get_newest():
     item_a = Clothing(age=2.0)
