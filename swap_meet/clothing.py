552eb6eb1d2f74e6d8b840f6212c6092bb384e2c
Rebecca Zuick <rebecca.zuick@gmail.com>
2021-09-30 15:39:21 -0500

    added age attribute and age_condition function

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,13 +1,17 @@
 from .item import Item
 
 class Clothing(Item):
-    def __init__(self, condition = 0):
+    def __init__(self, condition = 0, age = 0):
         super().__init__()
         self.category = "Clothing"
         self.condition = condition if condition else condition is 0
+        self.age = age if age else age is 0
     
     def __str__(self):
         return "The finest clothing you could wear."
     
     def condition_description(self):
         return super().condition_description()
+
+    def age_description(self):
+        return super().age_description()
