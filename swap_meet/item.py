c92bd5f50aa303f23a5db914c63e68b2db6a10e9
Vange Spracklin <houseofvange@gmail.com>
2021-09-30 16:56:53 -0700

    passes test wave 5

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -9,3 +9,18 @@ class Item:
 
     def __str__(self):
         return "Hello World!"
+
+    def condition_description(self):
+        if self.condition <= 1 :
+            return "POOR broken AND smells bad"
+        elif self.condition <= 2:
+            return "FAIR but smells bad..."
+        elif self.condition <= 3:
+            return "OK - doesn't stink!"
+        elif self.condition <= 4:
+            return "GOOD clean item"
+        elif self.condition <= 5:
+            return "EXCELLENT, smells of factory"
+        else:
+            return "fell off a truck... don't tell anyone where u got this"
+        
