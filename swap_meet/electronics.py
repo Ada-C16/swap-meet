7f81b8034f12121fecc0562e1606d63455798cf3
Reid Maute <reidhowdy@gmail.com>
2021-09-29 20:52:45 -0700

    Wave 5. All tests passing. Created method within parent class Item called condition_description() which returns the condition as an str.

--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -1,7 +1,7 @@
 from swap_meet.item import Item
 
 class Electronics(Item):
-    def __init__(self, condition = 0.0):
+    def __init__(self, condition = 0):
         super().__init__(category = "Electronics")
         self.condition = condition
     def __str__(self):
--- swap_meet/item.py
+++ swap_meet/item.py
@@ -7,6 +7,22 @@ class Item:
     def __str__(self):
         return "Hello World!"
 
+    def condition_description(self):
+        if self.condition == 1:
+            return "Ew."
+        elif self.condition == 2:
+            return "Um."
+        elif self.condition == 3:
+            return "I mean, I guess."
+        elif self.condition == 4:
+            return "Acceptable."
+        elif self.condition == 5:
+            return "Yes."
+        
+        #return self.condition_description
+
+
+
 
 
   
