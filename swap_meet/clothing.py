a5549c567d5d4622999256232a8a39dfd9d0c580
Asli Athman <asliathman@gmail.com>
2021-09-29 20:37:53 -0400

    added age attribute

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,8 +1,8 @@
 from swap_meet.item import Item
 
 class Clothing(Item):
-    def __init__(self, condition=0):
-        super().__init__(category="Clothing", condition=condition)
+    def __init__(self, condition=0, age=None):
+        super().__init__(category="Clothing", condition=condition, age=age)
 
     def __str__(self):
         return "The finest clothing you could wear."
