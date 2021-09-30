27641aac4ac45ed86b5add406262d9237a88eabc
kaitlyngore <kaitlyngore@gmail.com>
2021-09-30 12:17:22 -0400

    Vendor class: added swap_items method, passes wave 3 tests

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -19,3 +19,13 @@ class Vendor:
             if item.category == category:
                 item_list.append(item)
         return item_list
+
+    def swap_items(self, vendor, my_item, their_item):
+        if their_item not in vendor.inventory or my_item not in self.inventory:
+            return False
+        else:
+            vendor.inventory.remove(their_item)
+            self.inventory.append(their_item)
+            self.inventory.remove(my_item)
+            vendor.inventory.append(my_item)
+            return True
