59e236238c6bd688d2cafd94f4b48a9620c47ab7
Diana <diana.lozanocalderon@gmail.com>
2021-09-30 20:41:34 -0700

    All waves pass all the tests

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,9 +1,26 @@
 class Item:
-    
-    def __init__(self, category = ""):
+    #Item is a parent class. Inheritance!
+    def __init__(self, category = "", condition = 0):
             self.category = category
+            self.condition = condition
 
     def __str__(self):
         return "Hello World!"
+
+    def condition_description(self):
+        if self.condition > 0 and self.condition < 1:
+            return "This is garbage"
+        elif self.condition == 1:
+            return "Eh, you could do better"
+        elif self.condition ==2:
+            return "Not as bad as 1 but you can still do better"
+        elif self.condition == 3:
+            return "Not the best but not the worst"
+        elif self.condition == 4:
+            return "Much better, almost the best"
+        elif self.condition == 5:
+            return "This is the best you can get"
+        else:
+            return "What are you doing? Rating is not valid"
 
     
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -8,6 +8,7 @@ class Vendor:
         #self.item = Item()
 
     def add(self, item):
+        #Here Vendor becomes a composite and item becomes a component
         self.inventory.append(item)
         return item
 
@@ -50,7 +51,25 @@ class Vendor:
 
         return False
 
-#Wave 5
+#Wave 6
+    def get_best_by_category(self, category):
+        category_items = self.get_by_category(category)
+        if not category_items:
+            return None
+        best_item = category_items[0]
+        for item in category_items:
+            if item.condition > best_item.condition:
+                best_item = item
+        return best_item
+
+    def swap_best_by_category(self, other, my_priority, their_priority):
+        my_best_item = self.get_best_by_category(their_priority)
+        their_best_item = other.get_best_by_category(my_priority)
+        if not my_best_item or not their_best_item:
+            return False
+        self.swap_items(other, my_best_item, their_best_item)
+        return True
+
     
 
 
