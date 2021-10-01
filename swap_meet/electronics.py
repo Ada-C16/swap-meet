f467bcb701a728e8bb1cd50b31dde8020ea67069
Victoria Duke <toriduke@alumni.stanford.edu>
2021-09-30 23:07:02 -0700

    Creates Electronics class

--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -1,4 +1,8 @@
 from swap_meet.item import Item
 
 class Electronics(Item):
-    pass
+    def __init__(self, category = "Electronics", condition = 0):
+        super().__init__(category, condition)
+        
+    def __str__(self):
+        return "A gadget full of buttons and secrets."
