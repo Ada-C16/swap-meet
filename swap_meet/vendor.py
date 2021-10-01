74d7a1d8a541249bbc8b977cdf34c581494c0e76
Faith <fkauwe@gmail.com>
2021-09-30 21:44:28 -0700

    All tests passing, with enhancements and comments

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -50,19 +50,39 @@ class Vendor():
         if len(items_by_category)== 0:
             return None
         else:
+    #using list comprehension to compare just the condition attributes of the items objects
+    #in items_by_category list.  looking for the max value of these attributes
+    #storing the max value, a float, in the max_condition variable
             max_condition =max(item.condition for item in items_by_category)
+    #now going through items_by_category looking for which item's condition attribute
+    #matches max_condition and returning that whole item
             for item in items_by_category:
                 if item.condition == max_condition:
                     return item
 
     def swap_best_by_category(self, other, my_priority, their_priority):
-        
+    #using the get_best_by_category method twice, once for Vendor (self) and once for
+    #Vendor(other).  Store the results of those method calls in 2 separate variables    
         my_best = self.get_best_by_category(their_priority)
         their_best =other.get_best_by_category(my_priority)
         if my_best == None or their_best == None:
             return False
+    #call the swap_items method, using the 2 variables my_best, their_best
+    #as arguments
         else:
             self.swap_items(other, my_best, their_best)
+            return True
+    def swap_by_newest(self, other):
+    #using list comprehension to compare only the age attributes of all the item objects 
+    #in inventory and return the lowest value (for newest item). do this twice for self.inventory
+    #and other.inventory. Frankly, I'm amazed this works. Store results in 2 variables
+        my_newest = min(item.age for item in self.inventory)
+        their_newest = min(item.age for item in other.inventory)
+        if my_newest == None or their_newest == None:
+            return False
+    #call the swap_items method using 2 variables, my_newest, their newest
+        else:
+            self.swap_items(other, my_newest, their_newest)
             return True
 
 
