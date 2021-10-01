f537dca276bf70e888c2d57bc6e5bcb33e2763b4
Victoria Duke <toriduke@alumni.stanford.edu>
2021-10-01 00:21:06 -0700

    Creates methods get_best_by_category and swap_best_by_category in Vendor class

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -40,3 +40,18 @@ class Vendor:
         my_first_item = self.inventory[0]
         their_first_item = their_vendor.inventory[0]
         return self.swap_items(their_vendor, my_first_item, their_first_item)
+
+    def get_best_by_category(self, category):
+        best_item = None
+        if category not in [item.category for item in self.inventory]:
+            return None
+        for item in self.inventory:
+            if item.category == category:
+                if not best_item or item.condition > best_item.condition:
+                    best_item = item
+        return best_item
+    
+    def swap_best_by_category(self, other, my_priority, their_priority):
+        my_best_item = self.get_best_by_category(their_priority)
+        their_best_item = other.get_best_by_category(my_priority)
+        return self.swap_items(other, my_best_item, their_best_item)
