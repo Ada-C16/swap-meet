036f862cfba3ecbe861764112adcc7bf1bd352b0
Emily Colon <emiquigs@gmail.com>
2021-10-01 18:23:02 +1300

    In Item class, update 'is' to '==' in line 13

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,7 +1,8 @@
 class Item:
-    def __init__(self, category=None, condition = 0):
+    def __init__(self, category=None, condition = 0, age = 0):
         self.category = category if category is not None else ""
         self.condition = condition
+        self.age = age
     
     def __str__(self):
         return "Hello World!"
@@ -9,7 +10,7 @@ class Item:
     def condition_description(self):
         if self.condition < 2:
             return "This is heavily used."
-        if self.condition is 3-4:
+        if self.condition == 3-4:
             return "This is in mint condition."
         if self.condition >4:
             return "This is in excellent condition."
