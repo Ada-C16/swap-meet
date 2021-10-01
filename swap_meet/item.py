bc0cd5cc015e08ee3d99ef48c1f1e31c652b4966
Nicole Washington <nicolewashington.003@gmail.com>
2021-10-01 12:52:25 -0700

    waves 1-6 complete, comitting to submit (code not refactored*)

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,8 +1,27 @@
 class Item:
     #Wave 02
-    def __init__(self,category= ""):
+    def __init__(self,category= "", condition = 0):
         self.category = category 
+        self.condition = condition
     
     #Wave 03
     def __str__(self):
         return "Hello World!"
+
+    #Wave 05 
+    def condition_description(self ):
+        condition = self.condition
+        if condition >= 0 and condition < 1:
+            description = "yikes,raggedy, on its last leg"
+        if condition >= 1 and condition < 2:
+            description = "eek,heavy use,'rough around the edges'"
+        if condition >= 2 and condition < 3:
+            description = "eh,well used"
+        if condition >= 3 and condition < 4:
+            description = "ok,expected wear and tear"
+        if condition >= 4 and condition < 5:
+            description = "good,light use"
+        if condition == 5:
+            description = "superb,outstanding,like new"
+        return description
+        
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -8,26 +8,29 @@ class Vendor:
         '''
         input: one item
         - adds item to the inventory
-        output: item added 
+        output: returns item added 
         '''
         self.inventory.append(item)
         return item
     def remove(self,item):
         '''
         input: one item
-        - removes item from inventory if there is a mathcing item 
-        output: item removed , False if no matching item 
+        - if there is a mathcing item, removes item from inventory  
+        output: returns item removed if item is valid(matching item) 
+                else: returns False
         '''
         if item in self.inventory:
             self.inventory.remove(item)
             return item
         else:
             return False 
+
     #Wave 02
     def get_by_category(self,category):
         '''
         input: a string representing a category
-        output: a list of items in the inventory(list) with that category
+        output: returns list of items in the 
+        inventory(list) with that category
         '''
         result_list = []
         for item in self.inventory:
@@ -35,23 +38,19 @@ class Vendor:
                 result_list.append(item)
         return result_list
         
-        # return new list that is a sub-set of inventory list.
         
-    
-
 
     #Wave 03
     def swap_items(self,other_vendor, my_item,their_item):
         '''
-        input: other vendor(friend vendor is swappign with),
-        instance of Item (vendor plans to give), 
-        instance of Item (other vendor plans to give)
-        - removes my_item from this Vendor's inventory and adds 
-        it to friends/other vendor inventory
-        - removes their_item from friend/other Vendor inventory and adds
-        to this Vendors inventory 
-        output: True if items are in inventories , False if either item is
-        not in others inventory. 
+        input: instance of (other) Vendor,
+            instance of Item (this vendor plans to give), 
+            instance of Item (other vendor plans to give)
+        - if both items present in correspeonding Vendor inventory list
+            removes item from corresponding inventory list and adds to 
+            inventory list of oppisite vendor.
+        output: returns True: if items in list and swapped
+                else: returns False
         '''
         if my_item in self.inventory and their_item in other_vendor.inventory:
             self.remove(my_item)
@@ -67,12 +66,64 @@ class Vendor:
         
 
     #Wave 04
-    def swap_first_item(self,freind_vend):
-        pass
+    def swap_first_item(self,other_vendor):
+        '''
+        input: instance of (other) Vendor
+        - if both list are valid (not empty), switches first item of 
+            each other vendors and this vendor 
+            inventory list with each other. 
+        output: returns False: if either inventory empty
+                else: returns True 
+        '''
+        if  bool(self.inventory) == False or  bool(other_vendor.inventory) == False:
+            return False
+        else: 
+            my_first_item = self.inventory.pop(0)
+            their_first_item = other_vendor.inventory.pop(0)
+            self.inventory.insert(0, their_first_item)
+            other_vendor.inventory.insert(0, my_first_item)
+            return True
 
     #Wave 06
     def get_best_by_category(self,category):
-        pass
-    def swap_best_by_category(self,other_vendor, my_priority,their_priority):
-        pass
+        '''
+        input: a string that represents a category
+        - looks through inventory for item with matching category 
+            and best(highest value) condition. 
+        output: returns item: if there is a valid item (matching condition)
+                else: returns None
+        * returns sinlge item only, even if there
+                are same items with same condition in inventory.
+        '''
+        matching_category_list = self.get_by_category(category)
+        best_condition_num = 0
+        best_condition_item = None
+        if matching_category_list:
+            for item in matching_category_list:
+                if item.condition > best_condition_num:
+                    best_condition_num = item.condition
+                    best_condition_item = item
+            return best_condition_item
+        else: 
+            return None
+
+
+    def swap_best_by_category(self,other = "", my_priority= "",their_priority=""):
+        '''
+        input: instance of other Vendor, 
+            my_priority(category this vendor wants)
+            thier_priority(category other vendor wants to receive)
+        - if both vendors have item in inventory that match category of 
+            opposing vendor priority category: best items(greatest condition) 
+            that matches each vendors priority is swapped 
+            from invetory to oppossing vendors inventory
+        output : returns True : if matches found and items swapped
+                else: returns False 
+        '''
+        result = None
+        my_best_item = self.get_best_by_category(their_priority)  
+        their_best_item = other.get_best_by_category(my_priority)
+        result = self.swap_items(other, my_best_item,their_best_item)
+        return result
+
 
