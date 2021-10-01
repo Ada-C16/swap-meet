9240869629e0b7a0d5d41e69052c79c48429c1e5
Victoria Duke <toriduke@alumni.stanford.edu>
2021-09-30 21:41:08 -0700

    Creates __str__ method in Item class

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,3 +1,9 @@
 class Item:
     def __init__(self, category=""):
         self.category = category
+
+    def __str__(self):
+        if self.category == "":
+            return "Hello World!"
+        else:
+            return self.category
