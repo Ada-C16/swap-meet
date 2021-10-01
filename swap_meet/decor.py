e6b81edd04baab50670583f4c22c8bd57de523a4
Victoria Duke <toriduke@alumni.stanford.edu>
2021-09-30 23:06:35 -0700

    Creates Decor class

--- swap_meet/decor.py
+++ swap_meet/decor.py
@@ -1,4 +1,8 @@
 from swap_meet.item import Item
 
 class Decor(Item):
-    pass
+    def __init__(self, category = "Decor", condition = 0):
+        super().__init__(category, condition)
+        
+    def __str__(self):
+        return "Something to decorate your space."
