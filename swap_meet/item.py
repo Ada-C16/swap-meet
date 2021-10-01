a95e3c2ae49d6852d52649002065870a48e52fbe
Shaina Beth <shainabeth94@gmail.com>
2021-09-30 18:39:00 -0700

    Completed waves 1-5 and forgot to commit. Will try to remember to commit early and often

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,2 +1,29 @@
 class Item:
-    pass
+    def __init__(self, category=None, condition=None):
+        self.category = category if category is not None else ""
+        self.condition = condition if condition is not None else 0
+
+
+    def __str__(self):
+        return "Hello World!"
+
+
+    def condition_description(self):
+        if self.condition == 0:
+            return "trash"
+        
+        if self.condition == 1:
+            return "a memory of what it once was"
+
+        if self.condition == 2:
+            return "my ex left this"
+
+        if self.condition == 3:
+            return "good condition, but so last season"
+
+        if self.condition == 4:
+            return "the best you can do"
+
+        if self.condition == 5:
+            return "Beyonce owned this"
+            
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,49 @@
 class Vendor:
-    pass
+    def __init__(self, inventory=None):
+        self.inventory = inventory if inventory is not None else []
+    
+
+    def add(self, item):
+        self.inventory.append(item)
+        return item
+
+    def remove(self, item):
+        if item in self.inventory:
+            self.inventory.remove(item)
+            return item
+
+        return False
+
+    def get_by_category(self, category):
+        item_in_category = []
+        for item in self.inventory:
+            if item.category == category:
+                item_in_category.append(item)
+        
+        return item_in_category
+
+    def swap_items(self, vendor, my_item, their_item):
+        if my_item not in self.inventory or their_item not in vendor.inventory:
+            return False
+
+        self.add(their_item)
+        vendor.add(my_item)
+        self.remove(my_item)
+        vendor.remove(their_item)
+        
+        return True
+
+    def swap_first_item(self, vendor_friend):
+        if not self.inventory or not vendor_friend.inventory:
+            return False
+        
+        vendor_friend.inventory.append(self.inventory[0])
+        self.inventory.pop(0)
+        self.inventory.append(vendor_friend.inventory[0])
+        vendor_friend.inventory.pop(0)
+
+        return True
+
+
+
+
