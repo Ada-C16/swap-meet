ddd5bb88a234adfc4f86b01cdd68bfd586c0333a
Rebeca <muniz.movement@gmail.com>
2021-09-30 23:02:16 -0700

    Passing Wave 6

--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -3,9 +3,7 @@ from .item import Item
 class Electronics(Item):
     def __init__(self, condition = 0):
         super().__init__(category = "Electronics", condition = condition)
-        
-        
-
+         
     def __str__(self):
         return "A gadget full of buttons and secrets."
 
--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,31 +1,22 @@
 class Item:
-#This is value only class because there are no methods..technically we have the constructor. 
+
     def __init__(self, category = "", condition = 0):
         self.category = category
         self.condition = condition
         
-        
-        # if condition is None:
-        #     self.condition = 0.0
-        # else:
-        #     self.condition = condition
-        # if category is None:
-        #     self.category= ""
-        # else:
-        #     self.category=category
-    
- 
     def __str__(self):
         return "Hello World!"
 
     def condition_description(self):
-            if self.condition <= 1:
-                return("super used don't buy it")
-            elif self.condition <= 2:
-                return("pretty worn")
-            elif self.condition  <=3:
-                return("it's good for a date")
-            elif self.condition <=4:
-                return("Absolutely worth it")
-            elif self.condition <=5:
-                return("You bought it! YAY")
+        """This method describes the condition in words based on the value 0-5."""
+
+        if self.condition <= 1:
+            return("super used don't buy it")
+        elif self.condition <= 2:
+            return("pretty worn")
+        elif self.condition  <=3:
+            return("it's good for a date")
+        elif self.condition <=4:
+            return("Absolutely worth it")
+        elif self.condition <=5:
+            return("You bought it! YAY")
