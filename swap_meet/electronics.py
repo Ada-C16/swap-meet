b91e166939aad6517e3e7350d304d153ef3ba51c
Lux Barker <lux.barker13@gmail.com>
2021-09-30 18:20:04 -0700

    fix argument in super call

--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -3,7 +3,7 @@ from .item import Item
 
 class Electronics(Item):
     def __init__(self, condition=0, age=None):
-        super().__init__(condition, age=None, category="Electronics")
+        super().__init__(condition, age, category="Electronics")
 
     def __str__(self):
         return "A gadget full of buttons and secrets."
