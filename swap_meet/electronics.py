836aa30b8ead416c07638375c90f0cff030a266d
Ada <ada@Adas-MBP.hitronhub.home>
2021-10-01 09:41:02 -0700

    Leticia Cedar

--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -1,2 +1,8 @@
-class Electronics:
-    pass
+from .item import Item
+
+class Electronics(Item):
+    def __init__(self, name="", condition=0):
+        super().__init__(name, condition, category = "Electronics")
+
+    def __str__(self):
+        return "A gadget full of buttons and secrets."
