f581aeab81b464457b76c4917d4bd07431679ba4
syblades <86990570+syblades@users.noreply.github.com>
2021-10-01 10:51:31 -0700

    Implemented DRY to simplify the code

--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -2,9 +2,7 @@ from swap_meet.item import Item
 
 class Electronics(Item):
     def __init__(self, category = "Electronics", condition = 0):
-        self.category = category
-        self.condition = condition
-        
+        super().__init__(category, condition)
 
     def __str__(self):
         return "A gadget full of buttons and secrets."
