f35479c584f1cf953fbc1892bd27f8aeccf4d4cf
Lizet Tovar <lizett503@gmail.com>
2021-09-30 19:27:09 -0700

    Swap meet passing all waves

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,2 +1,24 @@
 class Item:
-    pass
+    def __init__(self, category=None, condition=0):
+        if not category:
+            category = ""
+        self.category = category
+        self.condition = condition
+        
+    def __str__(self):
+        return ("Hello World!")
+        
+
+    def condition_description(self):
+        if self.condition == 0:
+            return "One man's trash is another man's treasure."
+        elif self.condition == 1:
+            return "Very used and very discounted."
+        elif self.condition == 2:
+            return "Slightly ok condtion, slightly ok price."
+        elif self.condition == 3:
+            return "Ok condtion, ok price."
+        elif self.condition == 4:
+            return "Almost new!"
+        elif self.condition == 5: 
+            return "Like new, need the money."
