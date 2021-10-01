c078c09b268a674f5197e91e99b301e79f012809
Asha Paul <ashajpaul@gmail.com>
2021-09-30 20:31:10 -0400

    make changes to each file in the swap meet directory to pass all tests in the test files

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,2 +1,17 @@
 class Item:
-    pass
+    def __init__(self, category = "", condition = 0):
+        self.category = category
+        self.condition = condition
+
+    def __str__(self):
+        return "Hello World!"
+
+    def condition_description(self):
+        if 0 <= self.condition < 3:
+            return "This is in poor condition"
+        if 3 <= self.condition < 4:
+            return "This is in good condition"
+        if 4 <= self.condition < 5:
+            return "This is in very good condition"
+        if self.condition == 5:
+            return "This is in excellent condition"
