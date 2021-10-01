7ee0be2251f9116eed056b4c68c99af382e98853
syblades <86990570+syblades@users.noreply.github.com>
2021-10-01 01:53:53 -0700

    Refactored using Item hierarchy

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,4 +1,5 @@
 from swap_meet.item import Item
+
 class Clothing(Item):
     def __init__(self, category = "Clothing", condition = 0):
         self.category = category
@@ -6,17 +7,3 @@ class Clothing(Item):
 
     def __str__(self):
         return "The finest clothing you could wear."
-        
-    def condition_description(self):
-        if self.condition == 0:
-            return "This sucks."
-        elif self.condition == 1:
-            return "Eh, you can do better."
-        elif self.condition == 2:
-            return "This isn't the worst thing on Earth."
-        elif self.condition == 3:
-            return "This is almost average."
-        elif self.condition == 4:
-            return "This is pretty good quality."
-        elif self.condition == 5:
-            return "Nothing tops this!"
--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -1,4 +1,5 @@
 from swap_meet.item import Item
+
 class Electronics(Item):
     def __init__(self, category = "Electronics", condition = 0):
         self.category = category
@@ -7,18 +8,3 @@ class Electronics(Item):
 
     def __str__(self):
         return "A gadget full of buttons and secrets."
-    
-
-    def condition_description(self):
-        if self.condition == 0:
-            return "This sucks."
-        elif self.condition == 1:
-            return "Eh, you can do better."
-        elif self.condition == 2:
-            return "This isn't the worst thing on Earth."
-        elif self.condition == 3:
-            return "This is almost average."
-        elif self.condition == 4:
-            return "This is pretty good quality."
-        elif self.condition == 5:
-            return "Nothing tops this!"
