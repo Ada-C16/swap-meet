2230fa521d4e6bd96edf588e122d53de12e0c880
Juliana Ruiz <jruiz9312@gmail.com>
2021-10-03 10:02:22 -0700

    passing test case 5

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,2 +1,21 @@
-class Clothing:
-    pass
+from swap_meet.item import Item
+class Clothing(Item):
+
+
+    def __init__(self, condition = 0 ):
+        # super().__init__()
+        # since super() just means "get my parent class"
+        # this is equal to
+        # Item.__init__()
+        # which is defined in item.if self.condition == 3:py
+        self.category= "Clothing" 
+        self.condition = condition
+        # if condition: 
+        #     self.condition
+      
+    # automatically because of line 2
+    # you have the condition_description method from item.py
+    def __str__(self): 
+        return "The finest clothing you could wear."
+
+    
