a416ce040cb24f092ab1a2df4f4a23aa75e3b7e7
Aisha <ishaarigbabu@gmail.com>
2021-10-13 23:26:51 -0500

    wave 3 complete

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -12,11 +12,28 @@ class Vendor:
 
         result = self.inventory.pop(self.inventory.index(item))
         return result
+        
     def get_by_category(self, category_name):
         category = []
+        if not category_name:
+            return category
 
         for i in self.inventory:
             if category_name == i.category:
                 category.append(i)
 
         return category
+
+    def swap_items(self, vendor, my_item, their_item):
+        other_vendor = vendor
+
+        if my_item not in self.inventory or their_item not in other_vendor.inventory:
+            return False
+
+        their_swap = other_vendor.remove(their_item)
+        my_swap = self.remove(my_item)
+
+        self.add(their_swap)
+        other_vendor.add(my_swap)
+
+        return True
