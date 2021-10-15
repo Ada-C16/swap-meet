6088910ed697bc320ddb5a6020d99cb130a2a26a
Aisha <ishaarigbabu@gmail.com>
2021-10-15 10:05:10 -0500

    adds docstrings to electronics and other sub classes of item

--- swap_meet/decor.py
+++ swap_meet/decor.py
@@ -2,6 +2,11 @@ from swap_meet.item import Item
 
 class Decor(Item):
     def __init__(self, condition = 0, age = 0):
+        """
+        is a subclass of item, defines its own category attribute 
+        and condition, inherits method for condition description
+        from parent
+        """
         self.category = "Decor"
         self.condition = condition 
     def __str__(self):
--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -2,6 +2,11 @@ from swap_meet.item import Item
 
 class Electronics(Item):
     def __init__(self, condition = 0, age = 0):
+        """
+        is a subclass of item, defines its own category attribute 
+        and condition, inherits method for condition description
+        from parent
+        """
         self.category = "Electronics"
         self.condition = condition
         self.age = age
