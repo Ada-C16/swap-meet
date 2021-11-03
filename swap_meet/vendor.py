bb509720c9b1679409153935b413f1fd4ce046bc
Maria Obregon Garcia <mobregong@gmail.com>
2021-11-02 23:35:53 -0400

    Wave_01 passing, Vendor class created with inventory attribute and add and remove methods

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,14 @@
 class Vendor:
-    pass
+    def __init__(self, inventory = []):
+        self.inventory = inventory
+
+    def add(self, item):
+        updated_inventory = self.inventory.append(item)
+        return item
+
+    def remove(self, item):
+        if item in self.inventory:
+            updated_inventory = self.inventory.remove(item)
+        else: 
+            item = False
+        return item
